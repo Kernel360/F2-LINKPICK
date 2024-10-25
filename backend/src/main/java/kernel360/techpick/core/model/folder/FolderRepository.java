befeef6e53d79992071ccd1947729dac07fd2d4f
PSH <gyak404@gmail.com>
2024-10-25 14:24:17 +0900

    refactor: 정렬 로직 adaptor로 이관

--- backend/src/main/java/kernel360/techpick/core/model/folder/FolderRepository.java
+++ backend/src/main/java/kernel360/techpick/core/model/folder/FolderRepository.java
@@ -10,10 +10,6 @@ public interface FolderRepository extends JpaRepository<Folder, Long> {
 
 	List<Folder> findByParentFolderId(Long parentFolderId);
 
-	// TODO: Field 함수는 MySQL계열에서만 제공되므로, 이후 QueryDSL로 리팩토링 필요
-	@Query("select f from Folder f where f.id in :idList order by FIELD(f.id,:idList)")
-	List<Folder> findAllByIdListOrdered(List<Long> idList);
-
 	// TODO: QueryDSL 도입 후 리팩토링 필요
 	@Query("SELECT f FROM Folder f WHERE f.user.id = :userId AND f.folderType = kernel360.techpick.core.model.folder.FolderType.UNCLASSIFIED")
 	Folder findUnclassifiedByUserId(@Param("userId") Long userId);
