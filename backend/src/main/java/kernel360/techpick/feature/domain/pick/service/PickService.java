6abdafacc18d0afa38d751b994d09a48f890dbfe
sangwonsheep <sangwonsheep@gmail.com>
2024-10-25 16:18:42 +0900

    refactor: Pick Service 리팩토링 진행 및 임시 커밋

--- backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickService.java
+++ backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickService.java
@@ -5,13 +5,13 @@ import kernel360.techpick.feature.domain.pick.dto.PickResult;
 
 public interface PickService {
 
-    PickResult getPick(PickCommand.Read command);
+	PickResult getPick(PickCommand.Read command);
 
-    PickResult saveNewPick(PickCommand.Create command);
+	PickResult saveNewPick(PickCommand.Create command);
 
-    PickResult updatePick(PickCommand.Update command);
+	PickResult updatePick(PickCommand.Update command);
 
-    PickResult movePick(PickCommand.Move command);
+	void movePick(PickCommand.Move command);
 
-    void deletePick(PickCommand.Delete command);
+	void deletePick(PickCommand.Delete command);
 }
--- backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickServiceImpl.java
+++ backend/src/main/java/kernel360/techpick/feature/domain/pick/service/PickServiceImpl.java
@@ -8,20 +8,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kernel360.techpick.core.model.folder.Folder;
 import kernel360.techpick.core.model.pick.Pick;
+import kernel360.techpick.core.model.tag.Tag;
+import kernel360.techpick.feature.domain.folder.exception.ApiFolderException;
 import kernel360.techpick.feature.domain.pick.dto.PickCommand;
 import kernel360.techpick.feature.domain.pick.dto.PickMapper;
 import kernel360.techpick.feature.domain.pick.dto.PickResult;
+import kernel360.techpick.feature.domain.pick.exception.ApiPickException;
+import kernel360.techpick.feature.domain.tag.exception.ApiTagException;
 import kernel360.techpick.feature.infrastructure.folder.FolderAdaptor;
-import kernel360.techpick.feature.infrastructure.link.LinkAdaptor;
 import kernel360.techpick.feature.infrastructure.pick.PickAdaptor;
+import kernel360.techpick.feature.infrastructure.tag.TagAdaptor;
 import kernel360.techpick.feature.infrastructure.user.UserAdaptor;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PickServiceImpl implements PickService {
-	private final UserAdaptor userAdaptor;
-	private final LinkAdaptor linkAdaptor;
+
+	private final TagAdaptor tagAdaptor;
 	private final PickAdaptor pickAdaptor;
 	private final PickMapper pickMapper;
 	private final FolderAdaptor folderAdaptor;
@@ -29,72 +33,77 @@ public class PickServiceImpl implements PickService {
 	@Override
 	@Transactional(readOnly = true)
 	public PickResult getPick(PickCommand.Read command) {
-		var user = userAdaptor.getUser(command.userId());
-		var pick = pickAdaptor.getPick(user, command.pickId());
+		var pick = pickAdaptor.getPick(command.pickId());
+		checkPickUserAuthorization(command.userId(), pick);
 		return pickMapper.toReadResult(pick);
 	}
 
 	@Override
 	@Transactional
 	public PickResult saveNewPick(PickCommand.Create command) {
-		var user = userAdaptor.getUser(command.userId());
-		var folder = folderAdaptor.readFolder(user, command.parentFolderId());
-		var link = linkAdaptor.saveLink(command.linkInfo());
-		var pick = pickAdaptor.savePick(pickMapper.toEntity(command, user, folder, link));
+		var pick = pickAdaptor.savePick(command);
+		// folderAdaptor.readFolder();
+		// checkFolderUserAuthorization(command.userId(), )
+		// 링크가 본인 폴더인지 검증 필요
+		// 링크가 본인 링크인지 검증 필요
+
+		List<Long> tagOrderList = pick.getTagOrder();
+		List<Tag> tagList = tagAdaptor.getTagList(tagOrderList);
+		for (Tag tag : tagList) {
+			if (ObjectUtils.notEqual(tag.getUser(), pick.getUser())) {
+				throw ApiTagException.UNAUTHORIZED_TAG_ACCESS();
+			}
+			pickAdaptor.savePickTag(pick, tag);
+		}
+
 		return pickMapper.toCreateResult(pick);
 	}
 
 	@Override
 	@Transactional
 	public PickResult updatePick(PickCommand.Update command) {
-		var user = userAdaptor.getUser(command.userId());
-		var pick = pickAdaptor.getPick(user, command.pickId())
-							  .updateTitle(command.title()).updateMemo(command.memo());
-		updateNewTagIdList(pick, command.tagIdList());
-		return pickMapper.toUpdateResult(pick);
+		var pick = pickAdaptor.getPick(command.pickId());
+		checkPickUserAuthorization(command.userId(), pick);
+		return pickMapper.toUpdateResult(pickAdaptor.updatePick(command));
 	}
 
 	@Override
 	@Transactional
-	public PickResult movePick(PickCommand.Move command) {
-		var user = userAdaptor.getUser(command.userId());
-		var pick = pickAdaptor.getPick(user, command.pickId());
-		var destinationFolder = folderAdaptor.readFolder(user, command.parentFolderId());
-
-		if (isParentFolderChanged(pick.getParentFolder(), destinationFolder)) {
-			movePickToOtherFolder(pick, destinationFolder, command.orderIdx());
-			return pickMapper.toMoveResult(pick);
+	public void movePick(PickCommand.Move command) {
+		var pick = pickAdaptor.getPick(command.pickId());
+		checkPickUserAuthorization(command.userId(), pick);
+		if (isParentFolderChanged(pick.getParentFolder().getId(), command.destinationFolderId())) {
+			pickAdaptor.movePickToOtherFolder(command);
+			return;
 		}
-		pick.getParentFolder().updateChildPickOrder(command.pickId(), command.orderIdx());
-		return pickMapper.toMoveResult(pick);
+		pickAdaptor.movePickToCurrentFolder(command);
 	}
 
 	@Override
 	@Transactional
 	public void deletePick(PickCommand.Delete command) {
-		var user = userAdaptor.getUser(command.userId());
-		var pick = pickAdaptor.getPick(user, command.pickId());
-		pick.getParentFolder().removeChildPickOrder(command.pickId());
-		pickAdaptor.deletePick(pick);
+		var pick = pickAdaptor.getPick(command.pickId());
+		checkPickUserAuthorization(command.userId(), pick);
+		pickAdaptor.deletePick(command);
 	}
 
 	/**
 	 * Internal Helper Functions
 	 **/
-	private boolean isParentFolderChanged(Folder originalFolder, Folder destinationFolder) {
-		return ObjectUtils.notEqual(originalFolder, destinationFolder);
+	private boolean isParentFolderChanged(Long originalFolderId, Long destinationFolderId) {
+		return ObjectUtils.notEqual(originalFolderId, destinationFolderId);
 	}
 
-	private void movePickToOtherFolder(Pick pick, Folder destinationFolder, int orderIndex) {
-		pick.getParentFolder().removeChildPickOrder(pick.getId());
-		destinationFolder.updateChildPickOrder(pick.getId(), orderIndex);
-		pick.updateParentFolder(destinationFolder);
+	private void checkPickUserAuthorization(Long userId, Pick pick) {
+		if (ObjectUtils.notEqual(userId, pick.getUser().getId())) {
+			throw ApiPickException.PICK_UNAUTHORIZED_ACCESS();
+		}
 	}
 
-	private void updateNewTagIdList(Pick pick, List<Long> newTagOrderList) {
-		pick.getTagOrder().stream()
-			.filter(tagId -> !newTagOrderList.contains(tagId))
-			.forEach(tagId -> pickAdaptor.detachTagFromPick(pick, tagId));
-		pick.updateTagOrderList(newTagOrderList);
+	private void checkFolderUserAuthorization(Long userId, Folder folder) {
+		if (ObjectUtils.notEqual(userId, folder.getUser().getId())) {
+			throw ApiFolderException.FOLDER_ACCESS_DENIED();
+		}
 	}
+
 }
