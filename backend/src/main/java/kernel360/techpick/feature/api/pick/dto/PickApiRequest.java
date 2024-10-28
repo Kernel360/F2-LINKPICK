7ac20fff419d3b411a3e509acaded04d7922cfd8
sangwonsheep <sangwonsheep@gmail.com>
2024-10-28 17:56:41 +0900

    refactor: Pick Dto 수정

--- backend/src/main/java/kernel360/techpick/feature/api/pick/dto/PickApiRequest.java
+++ backend/src/main/java/kernel360/techpick/feature/api/pick/dto/PickApiRequest.java
@@ -1,9 +1,43 @@
 package kernel360.techpick.feature.api.pick.dto;
 
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import kernel360.techpick.feature.domain.link.dto.LinkInfo;
+
 public class PickApiRequest {
 
-    public record Create() {}
-    public record Read() {}
-    public record Update() {}
-    public record Delete() {}
+	public record Create(
+		String title,
+		String memo,
+		List<Long> tagOrderList,
+		Long parentFolderId,
+		LinkInfo linkInfo
+	) {
+	}
+
+	public record Read(
+		@NotNull Long pickId
+	) {
+	}
+
+	public record Update(
+		@NotNull Long pickId,
+		String title,
+		String memo,
+		List<Long> tagIdList
+	) {
+	}
+
+	public record Move(
+		@NotNull List<Long> pickIdList,
+		@NotNull Long destinationFolderId,
+		int orderIdx
+	) {
+	}
+
+	public record Delete(
+		@NotNull List<Long> pickIdList
+	) {
+	}
 }
