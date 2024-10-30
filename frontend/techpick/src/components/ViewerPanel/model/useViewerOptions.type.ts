2a85101c9c9f1b23c9e687c3eb743daad23df01d
kimminkyeu <minkyeki@student.42seoul.kr>
2024-10-30 11:26:06 +0900

    feat: 옵션 리스트 위젯 및 상태 구현

---
+++ frontend/techpick/src/components/ViewerPanel/model/useViewerOptions.type.ts
@@ -0,0 +1,28 @@
+import {
+  Filter,
+  FilterTemplateType,
+  InputType,
+} from '../template/filter/FilterTemplate';
+import {
+  ViewTemplate,
+  ViewTemplateType,
+} from '../template/view/ViewTemplate.type';
+
+export interface Options {
+  activeFilters: Filter[];
+  viewTemplate: ViewTemplate;
+}
+
+export interface FilterHandler {
+  addFilter: (type: FilterTemplateType, input: InputType) => void;
+  removeFilter: (type: FilterTemplateType, input: InputType) => void;
+}
+
+export interface ViewHandler {
+  changeView: (key: ViewTemplateType) => void;
+}
+
+export interface OptionsHandler {
+  filterHandler: FilterHandler;
+  viewHandler: ViewHandler;
+}
