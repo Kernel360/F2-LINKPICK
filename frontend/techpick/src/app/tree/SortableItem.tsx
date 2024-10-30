8cb437b40ac5a93f0cf55cc06d2b8d9322e683af
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-10-30 10:33:27 +0900

    refactor: handleClick 함수 분리

--- frontend/techpick/src/app/tree/SortableItem.tsx
+++ frontend/techpick/src/app/tree/SortableItem.tsx
@@ -2,9 +2,13 @@ import type { CSSProperties, MouseEvent } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { useTreeStore } from '@/stores/dndTreeStore/dndTreeStore';
-import { hasIndex } from '@/utils';
 import { draggableItem, draggingItem, selectedDragItemStyle } from './page.css';
-import { isSelectionActive } from './sortableItem.util';
+import {
+  getSelectedFolderRange,
+  isSameParentFolder,
+  isSelectionActive,
+} from './sortableItem.util';
+import type { FolderMapType } from '@/types';
 
 export function SortableItem({ id, name }: { id: number; name: string }) {
   const { treeDataMap, selectedFolderList, setSelectedFolderList, isDragging } =
@@ -22,46 +26,40 @@ export function SortableItem({ id, name }: { id: number; name: string }) {
       id: `test ${id}`,
     },
   });
-
   const isSelected = selectedFolderList.includes(id);
-
   const style: CSSProperties = {
     transform: CSS.Transform.toString(transform),
     transition,
     opacity: 1,
   };
 
+  const selectSingleFolder = (id: number) => {
+    setSelectedFolderList([id]);
+  };
+
+  const handleShiftSelect = (
+    id: number,
+    selectedList: number[],
+    treeDataMap: FolderMapType
+  ) => {
+    if (!isSameParentFolder(id, selectedList[0], treeDataMap)) {
+      selectSingleFolder(id);
+      return;
+    }
+
+    const newSelectedList = getSelectedFolderRange(
+      id,
+      selectedList,
+      treeDataMap
+    );
+    setSelectedFolderList(newSelectedList);
+  };
+
   const handleClick = (id: number, event: MouseEvent) => {
     if (event.shiftKey && isSelectionActive(selectedFolderList.length)) {
-      // 부모 폴더가 다르면 무시
-      if (
-        treeDataMap[id].parentFolderId !==
-        treeDataMap[selectedFolderList[0]].parentFolderId
-      ) {
-        setSelectedFolderList([id]);
-        return;
-      }
-
-      const parentFolderInfo = treeDataMap[treeDataMap[id].parentFolderId];
-      const lastSelectedIndex = parentFolderInfo.childFolderList.findIndex(
-        (item) => selectedFolderList.includes(item)
-      );
-      const currentIndex = parentFolderInfo.childFolderList.findIndex(
-        (item) => item === id
-      );
-
-      if (!hasIndex(lastSelectedIndex) || !hasIndex(currentIndex)) {
-        return;
-      }
-
-      const start = Math.min(lastSelectedIndex, currentIndex);
-      const end = Math.max(lastSelectedIndex, currentIndex);
-      const newSelectedFolderList = parentFolderInfo.childFolderList
-        .slice(start, end + 1)
-        .map((item) => item);
-      setSelectedFolderList(newSelectedFolderList);
+      handleShiftSelect(id, selectedFolderList, treeDataMap);
     } else {
-      setSelectedFolderList([id]);
+      selectSingleFolder(id);
     }
   };
 
