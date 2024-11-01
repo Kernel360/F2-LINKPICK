2b016e73fbe3eb046306867566261f9c2da5f054
PSH <gyak404@gmail.com>
2024-11-01 18:00:31 +0900

    fix: swagger에서 long userId 파라미터로 안받게 수정

--- backend/techpick-api/src/main/java/techpick/api/application/pick/controller/PickApiController.java
+++ backend/techpick-api/src/main/java/techpick/api/application/pick/controller/PickApiController.java
@@ -12,16 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import techpick.api.domain.pick.service.PickService;
+import lombok.RequiredArgsConstructor;
 import techpick.api.application.pick.dto.PickApiMapper;
 import techpick.api.application.pick.dto.PickApiRequest;
 import techpick.api.application.pick.dto.PickApiResponse;
+import techpick.api.domain.pick.service.PickService;
 import techpick.security.annotation.LoginUserId;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,7 +39,8 @@ public class PickApiController {
 		@ApiResponse(responseCode = "200", description = "픽 여부 조회 성공"),
 		@ApiResponse(responseCode = "404", description = "해당 링크에 대해 픽이 되어 있지 않습니다.")
 	})
-	public ResponseEntity<PickApiResponse> getPickUrl(@LoginUserId Long userId, @RequestParam String link) {
+	public ResponseEntity<PickApiResponse> getPickUrl(@Parameter(hidden = true) @LoginUserId Long userId,
+		@RequestParam String link) {
 		return ResponseEntity.ok(pickApiMapper.toApiResponse(pickService.getPickUrl(userId, link)));
 	}
 
@@ -47,7 +49,8 @@ public class PickApiController {
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "픽 상세 조회 성공")
 	})
-	public ResponseEntity<PickApiResponse> getPick(@LoginUserId Long userId, @PathVariable Long pickId) {
+	public ResponseEntity<PickApiResponse> getPick(@Parameter(hidden = true) @LoginUserId Long userId,
+		@PathVariable Long pickId) {
 		return ResponseEntity.ok(
 			pickApiMapper.toApiResponse(
 				pickService.getPick(pickApiMapper.toReadCommand(userId, new PickApiRequest.Read(pickId)))));
@@ -60,7 +63,7 @@ public class PickApiController {
 		@ApiResponse(responseCode = "401", description = "잘못된 태그 접근"),
 		@ApiResponse(responseCode = "403", description = "접근할 수 없는 폴더")
 	})
-	public ResponseEntity<PickApiResponse> savePick(@LoginUserId Long userId,
+	public ResponseEntity<PickApiResponse> savePick(@Parameter(hidden = true) @LoginUserId Long userId,
 		@Valid @RequestBody PickApiRequest.Create request) {
 		return ResponseEntity.ok(
 			pickApiMapper.toApiResponse(pickService.saveNewPick(pickApiMapper.toCreateCommand(userId, request))));
@@ -71,7 +74,7 @@ public class PickApiController {
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "픽 내용 수정 성공")
 	})
-	public ResponseEntity<PickApiResponse> updatePick(@LoginUserId Long userId,
+	public ResponseEntity<PickApiResponse> updatePick(@Parameter(hidden = true) @LoginUserId Long userId,
 		@Valid @RequestBody PickApiRequest.Update request) {
 		return ResponseEntity.ok(
 			pickApiMapper.toApiResponse(pickService.updatePick(pickApiMapper.toUpdateCommand(userId, request))));
@@ -83,7 +86,7 @@ public class PickApiController {
 		@ApiResponse(responseCode = "200", description = "픽 이동 성공"),
 		@ApiResponse(responseCode = "400", description = "폴더가 존재하지 않음.")
 	})
-	public ResponseEntity<Void> movePick(@LoginUserId Long userId,
+	public ResponseEntity<Void> movePick(@Parameter(hidden = true) @LoginUserId Long userId,
 		@Valid @RequestBody PickApiRequest.Move request) {
 		pickService.movePick(pickApiMapper.toMoveCommand(userId, request));
 		return ResponseEntity.ok().build();
@@ -96,7 +99,7 @@ public class PickApiController {
 		@ApiResponse(responseCode = "406", description = "휴지통이 아닌 폴더에서 픽 삭제 불가"),
 		@ApiResponse(responseCode = "500", description = "미확인 서버 에러 혹은 존재하지 않는 픽 삭제")
 	})
-	public ResponseEntity<Void> deletePick(@LoginUserId Long userId,
+	public ResponseEntity<Void> deletePick(@Parameter(hidden = true) @LoginUserId Long userId,
 		@Valid @RequestBody PickApiRequest.Delete request) {
 		pickService.deletePick(pickApiMapper.toDeleteCommand(userId, request));
 		return ResponseEntity.ok().build();
