0d454dc3593e5b013f9a702612177ffe1a6e6c06
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-05 16:57:48 +0900

    chore: mockdata 삭제

--- frontend/techpick/src/components/FolderTree/FolderTreeHeader.tsx
+++ frontend/techpick/src/components/FolderTree/FolderTreeHeader.tsx
@@ -1,11 +1,13 @@
-import { ROOT_FOLDER_ID } from '@/constants';
+import { useTreeStore } from '@/stores/dndTreeStore/dndTreeStore';
 import { folderTreeHeaderLayout } from './folderTreeHeader.css';
 import { ShowCreateFolderInputButton } from './ShowCreateFolderInputButton';
 
 export function FolderTreeHeader() {
+  const rootFolderId = useTreeStore((state) => state.rootFolderId);
+
   return (
     <div className={folderTreeHeaderLayout}>
-      <ShowCreateFolderInputButton newFolderParentId={ROOT_FOLDER_ID} />
+      <ShowCreateFolderInputButton newFolderParentId={rootFolderId} />
     </div>
   );
 }
