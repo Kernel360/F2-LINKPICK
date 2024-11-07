fb66bba22c6e1ea00113883e235cdd372c881d83
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-07 20:05:40 +0900

    feat: pickType 추가

---
+++ frontend/techpick/src/types/pick.type.ts
@@ -0,0 +1,30 @@
+import type { Concrete } from './util.type';
+import type { components } from '@/schema';
+
+export type PickInfoType = Concrete<
+  components['schemas']['techpick.api.domain.pick.dto.PickResult$Pick']
+>;
+
+export type PickInfoRecordType = {
+  [pickId: string]: PickInfoType | undefined;
+};
+
+export type PickIdOrderedListType = number[];
+
+export type PickRecordValueType = {
+  pickIdOrderedList: PickIdOrderedListType;
+  pickInfoRecord: PickInfoRecordType;
+};
+
+export type PickRecordType = {
+  [folderId: string]: PickRecordValueType | undefined;
+};
+
+export type PickListType = Concrete<
+  components['schemas']['techpick.api.domain.pick.dto.PickResult$Pick']
+>[];
+
+export type GetPicksByFolderIdResponseType = {
+  folderId: number;
+  pickList: PickListType;
+}[];
