ac025fa3f6ad09601952cff2339694fae1d6176b
kimminkyeu <minkyeki@student.42seoul.kr>
2024-11-11 17:43:50 +0900

    chore: 일부 주석 추가 및 코드 위치 변경

--- frontend/techpick/src/components/PickListViewerPanel/SearchWidget/SearchWidget.tsx
+++ frontend/techpick/src/components/PickListViewerPanel/SearchWidget/SearchWidget.tsx
@@ -46,20 +46,36 @@ export function TokenLabel(props: TokenLabelProps) {
 }
 
 export function SearchWidget() {
-  // tokenizer
+  // tokenizer + input
   const [input, setInput] = useState('');
   const inputRef = useRef<TokenInputRef>(null);
   const inputTokenizer = useMemo(
     () => getStringTokenizer<SearchKey>(pattern),
     []
   );
-  // inputs refs
+  // token list + current token context
   const [tokens, setTokens] = useState<Token<SearchKey>[]>([]);
   const [tokenInputContext, setTokenInputContext] =
     useState<Token<SearchKey> | null>(null);
-  // user data to use in recommendation
+  // fetched user data for auto-completion
   const { tagList, fetchingTagList } = useTagStore();
   const { getFolderList, getFolders } = useTreeStore();
+
+  /**
+   * @description
+   * 토큰 리스트에 변경이 일어나면, 실제 검색 api를 호출하기 위한 작업을 수행.
+   */
+  useEffect(
+    function fixSearchQueryOnTokenListChange() {
+      if (tokens.length <= 0) return;
+      console.log(tokens);
+      // TODO: convert token lists to global search state
+      // const tagList = tokens.filter((token) => token.key === 'TAG');
+      // const folderList = tokens.filter((token) => token.key === 'FOLDER');
+      // const textList = tokens.filter((token) => token.key === 'NONE');
+    },
+    [tokens]
+  );
 
   useEffect(
     function loadTagAndFolderList() {
@@ -79,15 +95,6 @@ export function SearchWidget() {
       currentToken && setTokenInputContext(currentToken);
     },
     [input, inputTokenizer]
-  );
-
-  useEffect(
-    function onTokenListChange() {
-      if (tokens.length <= 0) return;
-      console.log(tokens);
-      // TODO: convert token lists to global search state
-    },
-    [tokens]
   );
 
   const buildTokenIfNotAutocomplete = (input: string): Token<SearchKey> => {
@@ -150,6 +157,14 @@ export function SearchWidget() {
         style={{ display: 'none' }}
         value={tokenInputContext?.text}
       />
+      {/* TODO: 현재 Enter를 누르지 않고 다른 UI 클릭 시 그냥 토큰이 생성됩니다.
+            이 부분을 막아 주는 작업이 추가로 필요합니다.
+            -----------------------------------------------------------
+            재현 예시
+            - 1. #을 누르면 태그 리스트가 보여짐.
+            - 2. 그 상태로 다른 UI 창 클릭시, #이 그대로 입력 된다.
+            - 3. 또 자동 완성 아이템이 아닌, input 창을 클릭하면 #이 그대로 입력된다.
+            ----------------------------------------------------------- */}
       <WrappedTokenInput
         ref={inputRef}
         tokenValues={tokens}
