a6d6dd9594eff7d700a7295ed2e45764dad08eea
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-11 14:31:19 +0900

    refactor: DnDCurrentType을 dnd.type으로 이동

--- frontend/techpick/src/types/index.ts
+++ frontend/techpick/src/types/index.ts
@@ -2,3 +2,4 @@ export type { NodeData, DirectoryNodeProps } from './NodeData';
 export type * from './tag.type';
 export type * from './folder.type';
 export type * from './pick.type';
+export type * from './dnd.type';
