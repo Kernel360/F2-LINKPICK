64477927df6f08a72db95c31892f8c95efc41cea
kimminkyeu <minkyeki@student.42seoul.kr>
2024-11-11 17:16:44 +0900

    feat: 검색 위젯 토큰화 구현

--- frontend/techpick/src/components/PickListViewerPanel/SearchWidget/SearchWidget.tsx
+++ frontend/techpick/src/components/PickListViewerPanel/SearchWidget/SearchWidget.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Command } from 'cmdk';
+import { FolderOpen, Tag } from 'lucide-react';
+import WrappedTokenInput, {
+  KEY_DOWN_HANDLER_CONFIG_OPTION,
+  TokenInputRef,
+} from 'react-customize-token-input';
 import { SelectedTagItem } from '@/components';
 import { listItemStyle } from '@/components/PickListViewerPanel/SearchWidget/SearchWidget.css';
-import { getEntries } from '@/components/PickListViewerPanel/types/common.type';
-import {
-  TokenInfo,
-  TokenPrefixPattern,
-} from '@/components/PickListViewerPanel/util/tokenizer/PrefixTokenizer.type';
+import { Token } from '@/components/PickListViewerPanel/util/tokenizer/PrefixTokenizer.type';
 import { useTreeStore } from '@/stores/dndTreeStore/dndTreeStore';
 import { useTagStore } from '@/stores/tagStore';
 import { getStringTokenizer } from '../util';
 import { PrefixPatternBuilder } from '../util/tokenizer/PrefixPatternBuilder';
 import { FolderType, TagType } from '@/types';
+
+import './tokenInput.css';
 
 type SearchKey = 'TAG' | 'FOLDER' | 'NONE';
 
@@ -21,110 +24,164 @@ const pattern = new PrefixPatternBuilder<SearchKey>()
   .ifNoneMatch('NONE')
   .build();
 
-const findPrefixByKey = (
-  targetKey: SearchKey,
-  pattern: TokenPrefixPattern<SearchKey>
-) => {
-  const target = getEntries(pattern).find(([key, _]) => key === targetKey);
-  return target![1]; // target cannot be null
-};
+interface TokenLabelProps {
+  token: Token<SearchKey>;
+  icon?: React.ReactNode;
+}
 
-const initialAutocompleteContext: TokenInfo<SearchKey> = {
-  key: 'NONE',
-  token: '',
-};
+export function TokenLabel(props: TokenLabelProps) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: '4px',
+      }}
+    >
+      {props.icon}
+      {`${props.token.text}`}
+    </div>
+  );
+}
 
 export function SearchWidget() {
   // tokenizer
+  const [input, setInput] = useState('');
+  const inputRef = useRef<TokenInputRef>(null);
   const inputTokenizer = useMemo(
     () => getStringTokenizer<SearchKey>(pattern),
     []
   );
-  // basic inputs
-  const inputRef = useRef<HTMLInputElement>(null);
-  // last input Token
-  const [autocompleteContext, setAutocompleteContext] = useState<
-    TokenInfo<SearchKey>
-  >(initialAutocompleteContext);
-  // full input value
-  const [searchInput, setSearchInput] = useState('');
+  // inputs refs
+  const [tokens, setTokens] = useState<Token<SearchKey>[]>([]);
+  const [tokenInputContext, setTokenInputContext] =
+    useState<Token<SearchKey> | null>(null);
   // user data to use in recommendation
   const { tagList, fetchingTagList } = useTagStore();
-  // user data to use in recommendation
   const { getFolderList, getFolders } = useTreeStore();
 
-  useEffect(function loadTagAndFolderList() {
-    fetchingTagList(); // fetch tag list
-    getFolders(); // fetch folder list
-  }, []);
-
   useEffect(
-    function parseLastTokenOfSearchInput() {
-      const wordList = searchInput.split(' ');
-      const lastTokenInfo = inputTokenizer
-        .tokenize(wordList[wordList.length - 1])
-        .getLastTokenInfo();
-      setAutocompleteContext(lastTokenInfo ?? initialAutocompleteContext);
+    function loadTagAndFolderList() {
+      fetchingTagList(); // fetch tag list
+      getFolders(); // fetch folder list
     },
-    [searchInput]
+    [fetchingTagList, getFolders]
   );
 
-  const doInputAutoComplete = (item: TagType | FolderType) => () => {
-    const prefix = findPrefixByKey(autocompleteContext.key, pattern);
-    setSearchInput((prev) => {
-      const wordList = prev.split(' ');
-      wordList.pop();
-      wordList.push(`${prefix}${item.name}`);
-      return wordList.join(' ');
-    });
-    // close autocomplete context
-    setAutocompleteContext({ key: 'NONE', token: '' });
+  useEffect(
+    function setAutocompleteModeByInput() {
+      if (!input) {
+        resetInputContext();
+        return;
+      }
+      const currentToken = inputTokenizer.tokenize(input).getLastToken();
+      currentToken && setTokenInputContext(currentToken);
+    },
+    [input, inputTokenizer]
+  );
+
+  useEffect(
+    function onTokenListChange() {
+      if (tokens.length <= 0) return;
+      console.log(tokens);
+      // TODO: convert token lists to global search state
+    },
+    [tokens]
+  );
+
+  const buildTokenIfNotAutocomplete = (input: string): Token<SearchKey> => {
+    return {
+      key: 'NONE',
+      text: input,
+      id: -1,
+    };
+  };
+
+  const editTokenList = (newTokenValues: Token<SearchKey>[]) => {
+    setTokens(newTokenValues);
+    resetInputContext();
+  };
+
+  const renderTokenLabel = (token: Token<SearchKey>) => {
+    switch (token.key) {
+      case 'FOLDER':
+        return <TokenLabel token={token} icon={<FolderOpen size={'14px'} />} />;
+      case 'TAG':
+        return <TokenLabel token={token} icon={<Tag size={'14px'} />} />;
+      case 'NONE':
+      default:
+        return <>{`${token.text}`}</>;
+    }
+  };
+
+  const onAutocompleteSelect = (item: TagType | FolderType) => () => {
+    if (!tokenInputContext) return;
+    setTokens((prev) => [
+      ...prev,
+      { key: tokenInputContext.key, text: item.name, id: item.id },
+    ]);
+    resetInputContext();
+  };
+
+  const resetInputContext = () => {
+    setInput('');
+    setTokenInputContext(null);
+    inputRef.current?.setCreatorValue('');
     inputRef.current?.focus();
+  };
+
+  const setKeyEvent = (key?: SearchKey) => {
+    return key && key !== 'NONE'
+      ? { onEnter: KEY_DOWN_HANDLER_CONFIG_OPTION.OFF }
+      : { onEnter: KEY_DOWN_HANDLER_CONFIG_OPTION.ON };
   };
 
   return (
     <Command
+      style={{ width: '70%' }} // TODO: change to css.ts + 화면 크기에 따라 맞추기
       onClick={(e) => {
         e.stopPropagation();
         e.preventDefault();
       }}
       label={'Search'}
     >
-      <input // NOTE: 화면에 렌더링되는 Input
-        ref={inputRef}
-        // className={inputStyle}
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      <Command.Input // NOTE: 자동 완성 기능을 위한 Input 이며, 화면에 렌더링 되지 않음
+      <Command.Input // just for auto-completion
         style={{ display: 'none' }}
-        value={autocompleteContext.token}
+        value={tokenInputContext?.text}
+      />
+      <WrappedTokenInput
+        ref={inputRef}
+        tokenValues={tokens}
+        specialKeyDown={setKeyEvent(tokenInputContext?.key)}
+        onTokenValuesChange={editTokenList}
+        onBuildTokenValue={buildTokenIfNotAutocomplete}
+        onGetTokenDisplayLabel={renderTokenLabel}
+        onInputValueChange={setInput}
       />
       <Command.List>
-        <Command.Empty>No results found.</Command.Empty>
-        {autocompleteContext.key === 'TAG' &&
+        {tokenInputContext?.key === 'TAG' &&
           tagList.map((tag) => (
             <Command.Item
               key={tag.id}
               className={listItemStyle}
               value={tag.name}
-              onSelect={doInputAutoComplete(tag)}
+              onSelect={onAutocompleteSelect(tag)}
               keywords={[tag.name]}
             >
               <SelectedTagItem tag={tag} />
             </Command.Item>
           ))}
-        {autocompleteContext.key === 'FOLDER' &&
+        {tokenInputContext?.key === 'FOLDER' &&
           getFolderList().map((folder) => (
             <Command.Item
               key={folder.id}
               className={listItemStyle}
               value={folder.name}
-              onSelect={doInputAutoComplete(folder)}
+              onSelect={onAutocompleteSelect(folder)}
               keywords={[folder.name]}
             >
               {folder.name /* TODO: 폴더 아이템 컴포넌트로 수정 할 것 */}
-              {/*<FolderItem folder={folder} />*/}
             </Command.Item>
           ))}
       </Command.List>
