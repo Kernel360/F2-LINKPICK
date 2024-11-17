5731bf26dea1c3dba49bdab67abe2c9754604df8
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-17 17:46:32 +0900

    feat: theme 추가

--- frontend/techpick-extension/src/providers/ThemeProvider.tsx
+++ frontend/techpick-extension/src/providers/ThemeProvider.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import { useThemeStore } from '@/stores';
+import { useEffect, useRef } from 'react';
 import { lightTheme, darkTheme, commonThemeClass } from 'techpick-shared';
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const { isDarkMode } = useThemeStore();
+  const isLoadFirst = useRef<boolean>(false);
+  const getThemeModeFromLocalhost = useThemeStore(
+    (state) => state.getThemeModeFromLocalhost
+  );
+
+  useEffect(() => {
+    if (isLoadFirst.current === false) {
+      getThemeModeFromLocalhost();
+      isLoadFirst.current = true;
+    }
+  }, [getThemeModeFromLocalhost]);
 
   const currentTheme = isDarkMode ? darkTheme : lightTheme;
   return (
