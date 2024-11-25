6b76012a0168996e25d62056749a73b1be7e9bd8
Pak Su Hyung <gyak404@gmail.com>
2024-11-25 16:48:00 +0900

    fix: @MeasureTime 어노테이션 버그 수정 (#584)

--- backend/techpick-core/src/main/java/techpick/core/util/MeasureTimeAspect.java
+++ backend/techpick-core/src/main/java/techpick/core/util/MeasureTimeAspect.java
@@ -6,24 +6,29 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class MeasureTimeAspect {
+
+	private final RequestHolder requestHolder;
 
 	@Pointcut("@annotation(techpick.core.annotation.MeasureTime)")
 	public void pointcut() {
 	}
 
 	@Around("pointcut()")
-	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
 		var methodName = joinPoint.getSignature().getName();
 		// 필요하다면 StopWatch 클래스 도입 고려
 		long startTime = System.currentTimeMillis();
-		joinPoint.proceed();
+		var result = joinPoint.proceed();
 		long endTime = System.currentTimeMillis();
-		log.info("{} 실행 시간 : {} ms", methodName, endTime - startTime);
+		log.info("{} 실행 시간 : {} ms \n request : {} ", methodName, endTime - startTime, requestHolder.getRequest());
+		return result;
 	}
 }
