d3bd60ac4fdc35f84f357039ffffc8b798edda44
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-26 20:30:39 +0900

    fix: maximum call stack size exceeded 해결

--- frontend/techpick/src/components/PickContextMenu.tsx
+++ frontend/techpick/src/components/PickContextMenu.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+import { memo } from 'react';
 import type { PropsWithChildren } from 'react';
 import * as ContextMenu from '@radix-ui/react-context-menu';
 import { Trash2 as TrashIcon, CircleX as CircleXIcon } from 'lucide-react';
@@ -11,88 +10,87 @@ import {
 } from './pickContextMenu.css';
 import { PickInfoType } from '@/types';
 
-export function PickContextMenu({
-  pickInfo,
-  children,
-}: PropsWithChildren<PickContextMenuProps>) {
-  const portalContainer = getPortalContainer();
-  const { basicFolderMap } = useTreeStore();
-
-  const recycleBinFolderId = basicFolderMap
-    ? basicFolderMap['RECYCLE_BIN'].id
-    : -1;
-  const isRecycleBinFolder = recycleBinFolderId === pickInfo.parentFolderId;
-  const {
-    selectedPickIdList,
-    setSelectedPickIdList,
-    moveSelectedPicksToRecycleBinFolder,
-    deleteSelectedPicks,
-  } = usePickStore();
-
-  /**
-   * 1. 우클릭을 눌렀을 떄, 해당 id가 selectedId가 없다면 focus를 변경해야한다.
-   * 2. 있다면 넘어감 (multi-select)
-   * 3. selected를 한번에 넘긴다. 삭제면 삭제 요청.
-   * 휴지통 이동이면 moveFolders
-   */
-
-  const checkIsSelected = () => {
-    // 선택된게 아니라면
-    if (!selectedPickIdList.includes(pickInfo.id)) {
-      setSelectedPickIdList([pickInfo.id]);
-    }
-  };
-
-  return (
-    <ContextMenu.Root
-      onOpenChange={(open) => {
-        if (open) {
-          checkIsSelected();
-        }
-
-        return;
-      }}
-    >
-      <ContextMenu.Trigger data-pick-draggable={true}>
-        {children}
-      </ContextMenu.Trigger>
-      <ContextMenu.Portal container={portalContainer}>
-        <ContextMenu.Content
-          className={contextMenuContentLayout}
-          data-pick-draggable={true}
-        >
-          {isRecycleBinFolder ? (
-            <ContextMenu.Item
-              onSelect={() => {
-                deleteSelectedPicks({
-                  recycleBinFolderId,
-                });
-              }}
-              className={contextMenuItemStyle}
-            >
-              <CircleXIcon />
-              <p>삭제</p>
-            </ContextMenu.Item>
-          ) : (
-            <ContextMenu.Item
-              onSelect={() => {
-                moveSelectedPicksToRecycleBinFolder({
-                  picksParentFolderId: pickInfo.parentFolderId,
-                  recycleBinFolderId,
-                });
-              }}
-              className={contextMenuItemStyle}
-            >
-              <TrashIcon />
-              <p>휴지통으로 이동</p>
-            </ContextMenu.Item>
-          )}
-        </ContextMenu.Content>
-      </ContextMenu.Portal>
-    </ContextMenu.Root>
-  );
-}
-
 interface PickContextMenuProps {
   pickInfo: PickInfoType;
 }
+
+const PickContextMenu = memo(
+  function MemoizedPickContextMenu({
+    pickInfo,
+    children,
+  }: PropsWithChildren<PickContextMenuProps>) {
+    const portalContainer = getPortalContainer();
+    const { basicFolderMap } = useTreeStore();
+
+    const recycleBinFolderId = basicFolderMap
+      ? basicFolderMap['RECYCLE_BIN'].id
+      : -1;
+    const isRecycleBinFolder = recycleBinFolderId === pickInfo.parentFolderId;
+    const {
+      selectedPickIdList,
+      setSelectedPickIdList,
+      moveSelectedPicksToRecycleBinFolder,
+      deleteSelectedPicks,
+    } = usePickStore();
+
+    const checkIsSelected = () => {
+      if (!selectedPickIdList.includes(pickInfo.id)) {
+        setSelectedPickIdList([pickInfo.id]);
+      }
+    };
+
+    return (
+      <ContextMenu.Root
+        onOpenChange={(open) => {
+          if (open) {
+            checkIsSelected();
+          }
+        }}
+      >
+        <ContextMenu.Trigger data-pick-draggable={true}>
+          {children}
+        </ContextMenu.Trigger>
+        <ContextMenu.Portal container={portalContainer}>
+          <ContextMenu.Content
+            className={contextMenuContentLayout}
+            data-pick-draggable={true}
+          >
+            {isRecycleBinFolder ? (
+              <ContextMenu.Item
+                onSelect={() => {
+                  deleteSelectedPicks({
+                    recycleBinFolderId,
+                  });
+                }}
+                className={contextMenuItemStyle}
+              >
+                <CircleXIcon />
+                <p>삭제</p>
+              </ContextMenu.Item>
+            ) : (
+              <ContextMenu.Item
+                onSelect={() => {
+                  moveSelectedPicksToRecycleBinFolder({
+                    picksParentFolderId: pickInfo.parentFolderId,
+                    recycleBinFolderId,
+                  });
+                }}
+                className={contextMenuItemStyle}
+              >
+                <TrashIcon />
+                <p>휴지통으로 이동</p>
+              </ContextMenu.Item>
+            )}
+          </ContextMenu.Content>
+        </ContextMenu.Portal>
+      </ContextMenu.Root>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.pickInfo.id === nextProps.pickInfo.id;
+  }
+);
+
+PickContextMenu.displayName = 'PickContextMenu';
+
+export { PickContextMenu };
