a7b50bd189237d20427ddc0970eef86636a74502
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-27 19:32:17 +0900

    fix: TagPicker 잘 보이게 수정

--- frontend/techpick/src/components/pickDraggableListLayout.css.ts
+++ frontend/techpick/src/components/pickDraggableListLayout.css.ts
@@ -3,4 +3,6 @@ import { style } from '@vanilla-extract/css';
 export const pickDraggableListLayoutStyle = style({
   width: 'fit-content',
   overflowY: 'scroll',
+  /**paddingBottom이 없다면 TagPicker가 짤리게 됩니다.*/
+  paddingBottom: '160px',
 });
