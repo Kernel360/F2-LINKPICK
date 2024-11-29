4cd9f10c5e086b7f16274a79f949eb45adf6f4ac
kimminkyeu <minkyeki@student.42seoul.kr>
2024-11-29 15:50:31 +0900

    chore: 공유 폴더 예외 코드 추가

--- backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/exception/ApiSharedFolderErrorCode.java
+++ backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/exception/ApiSharedFolderErrorCode.java
@@ -7,53 +7,56 @@ import techpick.core.exception.level.ErrorLevel;
 
 public enum ApiSharedFolderErrorCode implements ApiErrorCode {
 
-	/**
-	 * Pick Error Code (PK)
-	 */
-	SHARED_FOLDER_NOT_FOUND
-		("SF-000", HttpStatus.NOT_FOUND, "존재하지 않는 SharedFolder", ErrorLevel.CAN_HAPPEN()),
-	SHARED_FOLDER_UNAUTHORIZED
-		("SF-001", HttpStatus.UNAUTHORIZED, "잘못된 SharedFolder 접근, 다른 사용자의 SharedFolder에 접근",
-			ErrorLevel.SHOULD_NOT_HAPPEN()),
-	;
+    /**
+     * Pick Error Code (PK)
+     */
+    SHARED_FOLDER_NOT_FOUND
+        ("SF-000", HttpStatus.NOT_FOUND, "존재하지 않는 SharedFolder", ErrorLevel.CAN_HAPPEN()),
+    SHARED_FOLDER_UNAUTHORIZED
+        ("SF-001", HttpStatus.UNAUTHORIZED, "잘못된 SharedFolder 접근, 다른 사용자의 SharedFolder에 접근",
+            ErrorLevel.SHOULD_NOT_HAPPEN()),
+    FOLDER_CANT_BE_SHARED
+        ("SF-002", HttpStatus.UNAUTHORIZED, "공유되어선 안되는 폴더가 공유되고 있습니다!",
+            ErrorLevel.MUST_NEVER_HAPPEN()),
+    ;
 
-	private final String code;
+    private final String code;
 
-	private final HttpStatus httpStatus;
+    private final HttpStatus httpStatus;
 
-	private final String errorMessage;
+    private final String errorMessage;
 
-	private final ErrorLevel logLevel;
+    private final ErrorLevel logLevel;
 
-	ApiSharedFolderErrorCode(String code, HttpStatus status, String message, ErrorLevel errorLevel) {
-		this.code = code;
-		this.httpStatus = status;
-		this.errorMessage = message;
-		this.logLevel = errorLevel;
-	}
+    ApiSharedFolderErrorCode(String code, HttpStatus status, String message, ErrorLevel errorLevel) {
+        this.code = code;
+        this.httpStatus = status;
+        this.errorMessage = message;
+        this.logLevel = errorLevel;
+    }
 
-	@Override
-	public String getCode() {
-		return this.code;
-	}
+    @Override
+    public String getCode() {
+        return this.code;
+    }
 
-	@Override
-	public String getMessage() {
-		return this.errorMessage;
-	}
+    @Override
+    public String getMessage() {
+        return this.errorMessage;
+    }
 
-	@Override
-	public HttpStatus getHttpStatus() {
-		return this.httpStatus;
-	}
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
 
-	@Override
-	public ErrorLevel getErrorLevel() {
-		return this.logLevel;
-	}
+    @Override
+    public ErrorLevel getErrorLevel() {
+        return this.logLevel;
+    }
 
-	@Override
-	public String toString() {
-		return convertCodeToString(this);
-	}
+    @Override
+    public String toString() {
+        return convertCodeToString(this);
+    }
 }
--- backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/exception/ApiSharedFolderException.java
+++ backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/exception/ApiSharedFolderException.java
@@ -5,15 +5,19 @@ import techpick.core.exception.base.ApiException;
 
 public class ApiSharedFolderException extends ApiException {
 
-	private ApiSharedFolderException(ApiErrorCode errorCode) {
-		super(errorCode);
-	}
+    private ApiSharedFolderException(ApiErrorCode errorCode) {
+        super(errorCode);
+    }
 
-	public static ApiSharedFolderException SHARED_FOLDER_NOT_FOUND() {
-		return new ApiSharedFolderException(ApiSharedFolderErrorCode.SHARED_FOLDER_NOT_FOUND);
-	}
+    public static ApiSharedFolderException SHARED_FOLDER_NOT_FOUND() {
+        return new ApiSharedFolderException(ApiSharedFolderErrorCode.SHARED_FOLDER_NOT_FOUND);
+    }
 
-	public static ApiSharedFolderException SHARED_FOLDER_UNAUTHORIZED() {
-		return new ApiSharedFolderException(ApiSharedFolderErrorCode.SHARED_FOLDER_UNAUTHORIZED);
-	}
+    public static ApiSharedFolderException SHARED_FOLDER_UNAUTHORIZED() {
+        return new ApiSharedFolderException(ApiSharedFolderErrorCode.SHARED_FOLDER_UNAUTHORIZED);
+    }
+
+    public static ApiSharedFolderException FOLDER_CANNOT_BE_SHARED() {
+        return new ApiSharedFolderException(ApiSharedFolderErrorCode.FOLDER_CANT_BE_SHARED);
+    }
 }
