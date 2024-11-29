fb0e7e3e103d41e1f0d72076217865efaecc81b1
kimminkyeu <minkyeki@student.42seoul.kr>
2024-11-29 15:53:22 +0900

    :sparkles: feat: 폴더 조회 시 공유 여부 확인 가능하도록 uuid 함께 반환 (accessToken)

--- backend/techpick-api/src/main/java/techpick/api/domain/folder/dto/FolderMapper.java
+++ backend/techpick-api/src/main/java/techpick/api/domain/folder/dto/FolderMapper.java
@@ -9,17 +9,12 @@ import techpick.core.model.folder.Folder;
 import techpick.core.model.user.User;
 
 @Mapper(
-	componentModel = "spring",
-	injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-	unmappedTargetPolicy = ReportingPolicy.ERROR
+    componentModel = "spring",
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface FolderMapper {
 
-	@Mapping(source = "id", target = "id")
-	@Mapping(source = "parentFolder.id", target = "parentFolderId")
-	@Mapping(source = "user.id", target = "userId")
-	@Mapping(source = "childFolderIdOrderedList", target = "childFolderIdOrderedList")
-	@Mapping(source = "childPickIdOrderedList", target = "childPickIdOrderedList")
-	FolderResult toResult(Folder folder);
-
+    @Mapping(source = "folder.parentFolder.id", target = "parentFolderId")
+    FolderResult toResult(Folder folder);
 }
