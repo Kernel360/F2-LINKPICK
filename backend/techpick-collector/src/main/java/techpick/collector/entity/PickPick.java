77d65b31e7f39b455922934180fa0c9fc34f21c8
sangwonsheep <sangwonsheep@gmail.com>
2024-11-30 15:14:36 +0900

    feat: mongodb 관련 엔티티

---
+++ backend/techpick-collector/src/main/java/techpick/collector/entity/PickPick.java
@@ -0,0 +1,36 @@
+package techpick.collector.entity;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "pick")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PickPick {
+
+	@Id
+	private String id;
+
+	private String url;
+
+	private String title;
+
+	private Long parentFolderId;
+
+	private List<Long> tagIdOrderedList;
+
+	@Builder
+	private PickPick(String url, String title, Long parentFolderId, List<Long> tagIdOrderedList) {
+		this.url = url;
+		this.title = title;
+		this.parentFolderId = parentFolderId;
+		this.tagIdOrderedList = tagIdOrderedList;
+	}
+}
