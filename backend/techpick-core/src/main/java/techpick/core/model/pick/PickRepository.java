4b3925915f291182cf01bcca6f0c0b0368212aa3
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-04 00:43:39 +0900

    fix: 1+N 쿼리 수정

--- backend/techpick-core/src/main/java/techpick/core/model/pick/PickRepository.java
+++ backend/techpick-core/src/main/java/techpick/core/model/pick/PickRepository.java
@@ -1,5 +1,6 @@
 package techpick.core.model.pick;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,16 +15,21 @@ import techpick.core.model.user.User;
 
 public interface PickRepository extends JpaRepository<Pick, Long> {
 
-	Optional<Pick> findByUserIdAndLinkUrl(Long userId, String url);
+    Optional<Pick> findByUserIdAndLinkUrl(Long userId, String url);
 
-	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
-	@QueryHints({
-		@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")
-	})
-	@Query("SELECT p FROM Pick p WHERE p.id = :id")
-	Optional<Pick> findByIdForUpdate(Long id);
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+        @QueryHint(name = "javax.persistence.lock.timeout", value = "3000")
+    })
+    @Query("SELECT p FROM Pick p WHERE p.id = :id")
+    Optional<Pick> findByIdForUpdate(Long id);
 
-	Optional<Pick> findByUserAndLink(User user, Link link);
+    Optional<Pick> findByUserAndLink(User user, Link link);
 
-	boolean existsByUserIdAndLink(Long userId, Link link);
+    boolean existsByUserIdAndLink(Long userId, Link link);
+
+    List<Pick> findAllByUserId(Long userId);
+
+    @Query("SELECT p from Pick p JOIN FETCH p.link WHERE p.id IN (:pickIdList)")
+    List<Pick> findAllById_JoinLink(List<Long> pickIdList);
 }
