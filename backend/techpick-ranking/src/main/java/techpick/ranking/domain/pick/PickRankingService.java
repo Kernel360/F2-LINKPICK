463fa4b3d3de4085d5a8496b3f68d841efb7053a
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-06 17:29:23 +0900

    chore: 주석 업데이트

--- backend/techpick-ranking/src/main/java/techpick/ranking/domain/pick/PickRankingService.java
+++ backend/techpick-ranking/src/main/java/techpick/ranking/domain/pick/PickRankingService.java
@@ -24,9 +24,13 @@ public class PickRankingService {
 
 	/**
 	 * @author minkyeu kim
-	 *     지금은 단순히 조회마다 연산을 해서 랭크표를 반환한다.
+	 *     ---------------------------------------------
+	 *     지금은 단순히 A~B 기간을 조회 후 연산을 해서 랭크표를 반환한다.
 	 *     1달 이상은 미리 집계해서 Monthy 테이블에 넣을 거나,
 	 *     저장 방식을 다르게 하도록 고민해야 함.
+	 *     ---------------------------------------------
+	 *     Ex. 일별 집계, 월별 집계, 연별 집계 테이블을 나눠서 미리 연산.
+	 *         API 호출은 일별, 월별, 연별을 나눠서 호출하도록 변경하면 해결 가능.
 	 */
 	@TechpickAnnotation.MeasureTime
 	public List<PickViewRankingResult> getLinksOrderByViewCount(LocalDate startDate, LocalDate endDate, int limit) {
