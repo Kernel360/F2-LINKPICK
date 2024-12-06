3e30894cc84bd35f19aaf3bc1589969cf2371289
Pak Su Hyung <gyak404@gmail.com>
2024-12-06 16:54:38 +0900

    feat: 로깅시 요청마다 UUID 부여 (#686)

--- backend/techpick-api/src/main/java/techpick/security/filter/RequestLoggingFilter.java
+++ backend/techpick-api/src/main/java/techpick/security/filter/RequestLoggingFilter.java
@@ -1,7 +1,9 @@
 package techpick.security.filter;
 
 import java.io.IOException;
+import java.util.UUID;
 
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +15,12 @@ import lombok.RequiredArgsConstructor;
 import techpick.core.util.CachedHttpServletRequest;
 import techpick.core.util.RequestHolder;
 
+/**
+ * 요청을 로깅하기 위한 Filter<br>
+ * UUID 를 MDC에 저장하여 Logback에서 활용<br>
+ * 요청 내용을 RequestHolder에 저장
+ * @author psh
+ * */
 @Component
 @RequiredArgsConstructor
 public class RequestLoggingFilter extends OncePerRequestFilter {
@@ -23,12 +31,14 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		try {
+			MDC.put("requestId", UUID.randomUUID().toString()); // for logback
 			CachedHttpServletRequest cachedRequest = new CachedHttpServletRequest(request);
 			requestHolder.setRequest(cachedRequest);
 			filterChain.doFilter(cachedRequest, response);
 		} finally {
 			// 요청이 마무리되면 무조건 ThreadLocal에 저장한 요청정보를 초기화
 			requestHolder.clearRequest();
+			MDC.remove("requestId"); // MDC 또한 ThreadLocal 기반이므로 반드시 해제해줘야함
 		}
 	}
 }
