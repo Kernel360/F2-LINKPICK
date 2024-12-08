d446da0a068609aab51faee82e9fc369edff35b0
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-08 22:12:08 +0900

    :sparkles: feat: OpenGraph 획득 클래스 분리

---
+++ backend/techpick-api/src/main/java/techpick/api/lib/opengraph/OpenGraphOption.java
@@ -0,0 +1,18 @@
+package techpick.api.lib.opengraph;
+
+import java.time.Duration;
+
+import lombok.Getter;
+
+@Getter
+public class OpenGraphOption {
+	private final String userAgent;
+	private final Duration httpRequestTimeoutyDuration;
+	private final String httpResponseDefaultCharsetName;
+
+	public OpenGraphOption(int timeoutSecond) {
+		httpRequestTimeoutyDuration = Duration.ofSeconds(timeoutSecond);
+		userAgent = "facebookexternalhit/1.1";
+		httpResponseDefaultCharsetName = "UTF-8";
+	}
+}
