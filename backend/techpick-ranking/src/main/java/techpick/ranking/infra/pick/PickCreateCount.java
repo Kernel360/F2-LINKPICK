cabafd79a9224a575ce59d3f98303c089f7bc141
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-09 01:14:01 +0900

    refactor: Mongo 엔티티 상속 관계 설정

---
+++ backend/techpick-ranking/src/main/java/techpick/ranking/infra/pick/PickCreateCount.java
@@ -0,0 +1,23 @@
+package techpick.ranking.infra.pick;
+
+import java.time.LocalDate;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+/**
+ * 해당 링크가 픽된 (=북마크화 된) 횟수를 날짜별로 집계합니다.
+ */
+@Document(collection = "link_picked_count")
+@Getter
+public class PickCreateCount extends UrlCount {
+
+	@Id
+	private String id;
+
+	public PickCreateCount(LocalDate date, String url) {
+		super(date, url);
+	}
+}
