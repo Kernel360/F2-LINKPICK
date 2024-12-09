21eda40c9fd226bd777537c77601082c1d55a69f
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-12-09 14:02:55 +0900

    faet: 로딩상태일 때 컴포넌트 추가

---
+++ frontend/techpick/src/app/(signed)/recommend/RecommendLoadingPage.tsx
@@ -0,0 +1,13 @@
+
+import { FolderContentLayout } from "@/components/FolderContentLayout";
+import { PickContentLayout } from "@/components/PickContentLayout";
+
+export function RecommendLoadingPage(){
+
+  return (
+    <FolderContentLayout>
+      <PickContentLayout>
+      </PickContentLayout>
+    </FolderContentLayout>
+  );
+}
