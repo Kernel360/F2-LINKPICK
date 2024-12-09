5701a6f55c2d7b932328cb3c12e568cf4bc0cf4d
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-09 21:30:37 +0900

    feat: 랭킹 모듈 서비스로 분리

--- backend/techpick-api/src/main/java/techpick/api/application/ranking/dto/RankingResponse.java
+++ backend/techpick-api/src/main/java/techpick/api/application/ranking/dto/RankingResponse.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record RankingByViewCount(
+public record RankingResponse(
 	@Schema(description = "오늘 하루 동안 인기 있는 링크 Top 10")
-	List<LinkInfoWithViewCount> dailyViewRanking,
+	List<LinkInfoWithCount> dailyViewRanking,
 
 	@Schema(description = "지난 7일동안 링크 조회 수 Top 10")
-	List<LinkInfoWithViewCount> weeklyViewRanking,
+	List<LinkInfoWithCount> weeklyViewRanking,
 
 	@Schema(description = "지난 30일동안 링크가 픽된 횟수 Top 10")
-	List<LinkInfoWithViewCount> monthlyPickRanking
+	List<LinkInfoWithCount> monthlyPickRanking
 ) {
 }
