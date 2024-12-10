7985329ff6b38b99f3b4ef7f7438e4ccd4571a6f
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-10 17:57:34 +0900

    refactor: 단순 미사용 메서드 제거

--- backend/techpick-ranking/src/main/java/techpick/ranking/infra/sharedFolder/SharedFolderPickViewCountRepository.java
+++ backend/techpick-ranking/src/main/java/techpick/ranking/infra/sharedFolder/SharedFolderPickViewCountRepository.java
@@ -1,22 +1,14 @@
 package techpick.ranking.infra.sharedFolder;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import techpick.ranking.infra.pick.PickViewCount;
 
 @Repository
 public interface SharedFolderPickViewCountRepository
 	extends MongoRepository<SharedFolderPickViewCount, String> {
 
 	Optional<SharedFolderPickViewCount> findSharedFolderPickViewCountByDateAndUrl(LocalDate date, String url);
-
-	List<PickViewCount> findByDateBetween(
-		LocalDate startDate,
-		LocalDate endDate
-	);
 }
