5feebd6a9acac7633ded53b508388418e7c79c2f
obvoso <autumninmoon@gmail.com>
2024-12-10 14:21:22 +0900

    fix: eslint

--- frontend/techpick/src/hooks/useSearchElementId.ts
+++ frontend/techpick/src/hooks/useSearchElementId.ts
@@ -1,5 +1,5 @@
+import { useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react';
 
 export default function useSearchElementId() {
   const searchElementId = useSearchParams().get('searchId');
