0589203566839d5a42047aee2d42d3caef58d703
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-11 18:47:14 +0900

    fix: 태그 배열 할당 문제 버그 수정

--- backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/service/SharedFolderService.java
+++ backend/techpick-api/src/main/java/techpick/api/domain/sharedFolder/service/SharedFolderService.java
@@ -32,136 +32,136 @@ import techpick.core.model.sharedFolder.SharedFolder;
 @RequiredArgsConstructor
 public class SharedFolderService {
 
-    private final SharedFolderDataHandler sharedFolderDataHandler;
-    private final SharedFolderMapper sharedFolderMapper;
+	private final SharedFolderDataHandler sharedFolderDataHandler;
+	private final SharedFolderMapper sharedFolderMapper;
 
-    private final FolderDataHandler folderDataHandler;
-    private final TagDataHandler tagDataHandler;
-    private final PickDataHandler pickDataHandler;
+	private final FolderDataHandler folderDataHandler;
+	private final TagDataHandler tagDataHandler;
+	private final PickDataHandler pickDataHandler;
 
-    private final LinkMapper linkMapper;
+	private final LinkMapper linkMapper;
 
-    @Transactional
-    public SharedFolderResult.Create createSharedFolder(Long userId, Long sourceFolderId) {
-        var folder = folderDataHandler.getFolder(sourceFolderId);
-        validateUserIsFolderOwner(userId, folder);
-        validateFolderIsNotShared(folder);
-        validateFolderIsGeneralType(folder);
-        var sharedFolder = sharedFolderDataHandler.save(userId, sourceFolderId);
-        return sharedFolderMapper.toCreateResult(sharedFolder);
-    }
+	@Transactional
+	public SharedFolderResult.Create createSharedFolder(Long userId, Long sourceFolderId) {
+		var folder = folderDataHandler.getFolder(sourceFolderId);
+		validateUserIsFolderOwner(userId, folder);
+		validateFolderIsNotShared(folder);
+		validateFolderIsGeneralType(folder);
+		var sharedFolder = sharedFolderDataHandler.save(userId, sourceFolderId);
+		return sharedFolderMapper.toCreateResult(sharedFolder);
+	}
 
-    /**
-     * @author minkyeu kim
-     * 지금 코드가 복잡한데, 반드시 추후 리팩토링 진행해야 한다.
-     * 일단 DB에서 최신 정보를 끌어오지만, Update가 자주 일어나지 않는다는 가정 하에
-     * 정보를 캐싱해두는 쪽으로 개선할 수 있을 듯 하다.
-     *
-     * @implNote
-     * 폴더 내 Tag 정보 리스트를 두고, 그 리스트의 index로 픽의 태그를 표현한다.
-     * Ex. tagList = { "foo", "bar", "hi" } 일 때
-     *     pick.tagIdxList = [0, 2, 1] 이면
-     *     pick에 설정된 태그는 [ "foo", "hi", "bar" ] 이다.
-     */
-    @Transactional(readOnly = true)
-    @TechpickAnnotation.MeasureTime
-    public SharedFolderResult.SharedFolderInfo getSharedFolderInfo(UUID uuid) {
-        var sourceFolder = sharedFolderDataHandler.getByUUID(uuid).getFolder();
-        var pickList = pickDataHandler.getPickList(sourceFolder.getChildPickIdOrderedList());
-        var alternativeTagIdMap = createAlternativeTagIdMap(pickList);
+	/**
+	 * @author minkyeu kim
+	 * 지금 코드가 복잡한데, 반드시 추후 리팩토링 진행해야 한다.
+	 * 일단 DB에서 최신 정보를 끌어오지만, Update가 자주 일어나지 않는다는 가정 하에
+	 * 정보를 캐싱해두는 쪽으로 개선할 수 있을 듯 하다.
+	 *
+	 * @implNote
+	 * 폴더 내 Tag 정보 리스트를 두고, 그 리스트의 index로 픽의 태그를 표현한다.
+	 * Ex. tagList = { "foo", "bar", "hi" } 일 때
+	 *     pick.tagIdxList = [0, 2, 1] 이면
+	 *     pick에 설정된 태그는 [ "foo", "hi", "bar" ] 이다.
+	 */
+	@Transactional(readOnly = true)
+	@TechpickAnnotation.MeasureTime
+	public SharedFolderResult.SharedFolderInfo getSharedFolderInfo(UUID uuid) {
+		var sourceFolder = sharedFolderDataHandler.getByUUID(uuid).getFolder();
+		var pickList = pickDataHandler.getPickListPreservingOrder(sourceFolder.getChildPickIdOrderedList());
+		var alternativeTagIdMap = createAlternativeTagIdMap(pickList);
 
-        return SharedFolderResult.SharedFolderInfo
-            .builder()
-            .folderName(sourceFolder.getName())
-            .createdAt(sourceFolder.getCreatedAt())
-            .updatedAt(sourceFolder.getUpdatedAt())
-            .pickList(createSharedPickList(pickList, alternativeTagIdMap))
-            .tagList(createSharedTagList(alternativeTagIdMap))
-            .build();
-    }
+		return SharedFolderResult.SharedFolderInfo
+			.builder()
+			.folderName(sourceFolder.getName())
+			.createdAt(sourceFolder.getCreatedAt())
+			.updatedAt(sourceFolder.getUpdatedAt())
+			.pickList(createSharedPickList(pickList, alternativeTagIdMap))
+			.tagList(createSharedTagList(alternativeTagIdMap))
+			.build();
+	}
 
-    @Transactional(readOnly = true)
-    public Optional<String> findFolderAccessTokenByFolderId(Long folderId) {
-        return sharedFolderDataHandler.findUUIDBySourceFolderId(folderId).map(UUID::toString);
-    }
+	@Transactional(readOnly = true)
+	public Optional<String> findFolderAccessTokenByFolderId(Long folderId) {
+		return sharedFolderDataHandler.findUUIDBySourceFolderId(folderId).map(UUID::toString);
+	}
 
-    @Transactional(readOnly = true)
-    public List<SharedFolderResult.Read> getSharedFolderListByUserId(Long userId) {
-        return sharedFolderDataHandler.getByUserId(userId).stream()
-                                      .map(sharedFolderMapper::toReadResult).toList();
-    }
+	@Transactional(readOnly = true)
+	public List<SharedFolderResult.Read> getSharedFolderListByUserId(Long userId) {
+		return sharedFolderDataHandler.getByUserId(userId).stream()
+									  .map(sharedFolderMapper::toReadResult).toList();
+	}
 
-    @Transactional
-    public void deleteSharedFolder(Long userId, Long sourceFolderId) {
-        SharedFolder sharedFolder = sharedFolderDataHandler.getByFolderId(sourceFolderId);
-        validateUserIsSharedFolderOwner(userId, sharedFolder);
-        sharedFolderDataHandler.deleteBySourceFolderId(sourceFolderId);
-    }
+	@Transactional
+	public void deleteSharedFolder(Long userId, Long sourceFolderId) {
+		SharedFolder sharedFolder = sharedFolderDataHandler.getByFolderId(sourceFolderId);
+		validateUserIsSharedFolderOwner(userId, sharedFolder);
+		sharedFolderDataHandler.deleteBySourceFolderId(sourceFolderId);
+	}
 
-    private void validateFolderIsNotShared(Folder folder) {
-        if (sharedFolderDataHandler.isSharedFolder(folder.getId())) {
-            throw ApiSharedFolderException.FOLDER_ALREADY_SHARED();
-        }
-    }
+	private void validateFolderIsNotShared(Folder folder) {
+		if (sharedFolderDataHandler.isSharedFolder(folder.getId())) {
+			throw ApiSharedFolderException.FOLDER_ALREADY_SHARED();
+		}
+	}
 
-    private void validateUserIsFolderOwner(Long userId, Folder folder) {
-        if (!folder.getUser().getId().equals(userId)) {
-            throw ApiFolderException.FOLDER_ACCESS_DENIED();
-        }
-    }
+	private void validateUserIsFolderOwner(Long userId, Folder folder) {
+		if (!folder.getUser().getId().equals(userId)) {
+			throw ApiFolderException.FOLDER_ACCESS_DENIED();
+		}
+	}
 
-    private void validateUserIsSharedFolderOwner(Long userId, SharedFolder sharedFolder) {
-        if (!sharedFolder.getUser().getId().equals(userId)) {
-            throw ApiSharedFolderException.SHARED_FOLDER_UNAUTHORIZED();
-        }
-    }
+	private void validateUserIsSharedFolderOwner(Long userId, SharedFolder sharedFolder) {
+		if (!sharedFolder.getUser().getId().equals(userId)) {
+			throw ApiSharedFolderException.SHARED_FOLDER_UNAUTHORIZED();
+		}
+	}
 
-    private void validateFolderIsGeneralType(Folder folder) {
-        if (!folder.getFolderType().equals(FolderType.GENERAL)) {
-            throw ApiSharedFolderException.FOLDER_CANNOT_BE_SHARED();
-        }
-    }
+	private void validateFolderIsGeneralType(Folder folder) {
+		if (!folder.getFolderType().equals(FolderType.GENERAL)) {
+			throw ApiSharedFolderException.FOLDER_CANNOT_BE_SHARED();
+		}
+	}
 
-    private Map<Long, Integer> createAlternativeTagIdMap(List<Pick> pickList) {
-        Map<Long, Integer> alternativeIdMap = new HashMap<>();
-        for (var pick : pickList) {
-            for (var tagId : pick.getTagIdOrderedList()) {
-                if (!alternativeIdMap.containsKey(tagId)) {
-                    var nextId = alternativeIdMap.size(); // (0~N) incremental value
-                    alternativeIdMap.put(tagId, nextId);
-                }
-            }
-        }
-        return alternativeIdMap;
-    }
+	private Map<Long, Integer> createAlternativeTagIdMap(List<Pick> pickList) {
+		Map<Long, Integer> alternativeIdMap = new HashMap<>();
+		for (var pick : pickList) {
+			for (var tagId : pick.getTagIdOrderedList()) {
+				if (!alternativeIdMap.containsKey(tagId)) {
+					var nextId = alternativeIdMap.size(); // (0~N) incremental value
+					alternativeIdMap.put(tagId, nextId);
+				}
+			}
+		}
+		return alternativeIdMap;
+	}
 
-    private List<SharedFolderResult.SharedTagInfo> createSharedTagList(Map<Long, Integer> alternativeTagIdMap) {
-        var matchingTagList = tagDataHandler.getTagList(alternativeTagIdMap.keySet().stream().toList());
-        var initialSize = alternativeTagIdMap.size();
-        List<SharedFolderResult.SharedTagInfo> sharedTagInfoList = new ArrayList<>(initialSize);
-        for (var tag : matchingTagList) {
-            var newId = alternativeTagIdMap.get(tag.getId());
-            var sharedTagInfo = sharedFolderMapper.toSharedTagInfo(tag);
-            sharedTagInfoList.add(newId, sharedTagInfo);
-        }
-        return sharedTagInfoList;
-    }
+	private List<SharedFolderResult.SharedTagInfo> createSharedTagList(Map<Long, Integer> alternativeTagIdMap) {
+		var matchingTagList = tagDataHandler.getTagList(alternativeTagIdMap.keySet().stream().toList());
+		var initialSize = alternativeTagIdMap.size();
+		var sharedTagInfoArr = new SharedFolderResult.SharedTagInfo[initialSize];
+		for (var tag : matchingTagList) {
+			var newId = alternativeTagIdMap.get(tag.getId());
+			var sharedTagInfo = sharedFolderMapper.toSharedTagInfo(tag);
+			sharedTagInfoArr[newId] = sharedTagInfo;
+		}
+		return List.of(sharedTagInfoArr);
+	}
 
-    private List<SharedFolderResult.SharedPickInfo> createSharedPickList(
-        List<Pick> pickList,
-        Map<Long, Integer> alternativeTagIdMap
-    ) {
-        return pickList
-            .stream()
-            .map((pick) ->
-                SharedFolderResult.SharedPickInfo
-                    .builder()
-                    .title(pick.getTitle())
-                    .linkInfo(linkMapper.of(pick.getLink()))
-                    .tagIdxList(pick.getTagIdOrderedList().stream().map(alternativeTagIdMap::get).toList())
-                    .createdAt(pick.getCreatedAt())
-                    .updatedAt(pick.getUpdatedAt())
-                    .build()
-            ).toList();
-    }
+	private List<SharedFolderResult.SharedPickInfo> createSharedPickList(
+		List<Pick> pickList,
+		Map<Long, Integer> alternativeTagIdMap
+	) {
+		return pickList
+			.stream()
+			.map((pick) ->
+				SharedFolderResult.SharedPickInfo
+					.builder()
+					.title(pick.getTitle())
+					.linkInfo(linkMapper.of(pick.getLink()))
+					.tagIdxList(pick.getTagIdOrderedList().stream().map(alternativeTagIdMap::get).toList())
+					.createdAt(pick.getCreatedAt())
+					.updatedAt(pick.getUpdatedAt())
+					.build()
+			).toList();
+	}
 }
