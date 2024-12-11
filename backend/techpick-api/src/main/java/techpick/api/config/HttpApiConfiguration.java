164246dfaa1addc97aa6165cb979e322d9c5c03e
Sangwon Yang <sangwonsheep@gmail.com>
2024-12-11 13:05:03 +0900

    refactor: RestClient 타임 아웃 설정 (#736)

--- backend/techpick-api/src/main/java/techpick/api/config/HttpApiConfiguration.java
+++ backend/techpick-api/src/main/java/techpick/api/config/HttpApiConfiguration.java
@@ -1,8 +1,13 @@
 package techpick.api.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,6 +17,8 @@ import techpick.api.infrastructure.ranking.RankingApi;
 /**
  * 외부 서버와 통신하는 것을 Http Interface 방식으로 사용하기 위한 설정. <br>
  * - https://docs.spring.io/spring-framework/reference/integration/rest-clients.html#rest-http-interface
+ * 타임 아웃 설정
+ * - https://www.baeldung.com/spring-rest-timeout
  */
 @Configuration
 public class HttpApiConfiguration {
@@ -20,10 +27,26 @@ public class HttpApiConfiguration {
 	private String rankingServerUrl;
 
 	@Bean
-	public RankingApi rankingApi() {
-		var restClient = RestClient.create(rankingServerUrl);
+	public RankingApi rankingApi(RestClient restClient) {
 		var adapter = RestClientAdapter.create(restClient);
 		var proxy = HttpServiceProxyFactory.builderFor(adapter).build();
 		return proxy.createClient(RankingApi.class);
+	}
+
+	@Bean
+	public RestClient restClient() {
+		return RestClient
+			.builder()
+			.baseUrl(rankingServerUrl)
+			.requestFactory(clientHttpRequestFactory())
+			.build();
+	}
+
+	@Bean
+	public ClientHttpRequestFactory clientHttpRequestFactory() {
+		ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS
+			.withConnectTimeout(Duration.ofMillis(500))
+			.withReadTimeout(Duration.ofMillis(500));
+		return ClientHttpRequestFactories.get(settings);
 	}
 }
