567bc6b197fdcdafd56e6a41f991aabc654a7535
sangwonsheep <sangwonsheep@gmail.com>
2024-12-11 11:43:42 +0900

    refactor: 주석 변경

--- backend/techpick-api/src/main/java/techpick/api/domain/ranking/service/RankingService.java
+++ backend/techpick-api/src/main/java/techpick/api/domain/ranking/service/RankingService.java
@@ -27,6 +27,11 @@ public class RankingService {
 		var past7DaysViewRanking = // 일주일 전 ~ 어제
 			rankingApi.getUrlRankingByViewCount(before7Days, before1Day, limit).getBody();
 
+		/**
+		 * @author sangwon
+		 * 어제가 아닌 오늘로 변경한 이유는 가장 많이 저장한 픽을 즉시 확인하기 위함.
+		 * 집계 테이블이 추가되고, 캐싱 처리한다면 오늘 데이터는 즉시 보여줄 필요가 없기 때문에 어제부터로 하는 것은 어떨지? - psh
+		 */
 		var past30DaysPickRanking = // 한달 전 ~ 오늘
 			rankingApi.getUrlRankingByPickedCount(before30Days, currentDay, limit).getBody();
 
