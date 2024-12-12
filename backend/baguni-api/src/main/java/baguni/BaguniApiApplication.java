d049633ecc1bcffa1d7f1d0409d8c63550696ff2
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-13 03:11:32 +0900

    chore: 배포 액션 트리거를 위한 변경

--- backend/baguni-api/src/main/java/baguni/BaguniApiApplication.java
+++ backend/baguni-api/src/main/java/baguni/BaguniApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaguniApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BaguniApiApplication.class, args);
 	}
