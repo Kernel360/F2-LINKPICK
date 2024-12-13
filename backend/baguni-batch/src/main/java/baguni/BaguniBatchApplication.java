d08f07cef62965f527a59d092555949af2f454a4
kimminkyeu <minkyeki@student.42seoul.kr>
2024-12-13 11:12:52 +0900

    chore: 액션 실행을 위한 공백 추가

--- backend/baguni-batch/src/main/java/baguni/BaguniBatchApplication.java
+++ backend/baguni-batch/src/main/java/baguni/BaguniBatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaguniBatchApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BaguniBatchApplication.class, args);
 	}
