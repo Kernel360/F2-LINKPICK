9685b7e4949ecb0ee86a3a68d6dcc9bd114af861
sangwonsheep <sangwonsheep@gmail.com>
2024-12-13 18:16:43 +0900

    refactor: 픽 리스트 조회에 붙였던 락 제거

--- backend/baguni-api/src/main/java/baguni/api/domain/pick/service/PickService.java
+++ backend/baguni-api/src/main/java/baguni/api/domain/pick/service/PickService.java
@@ -83,7 +83,6 @@ public class PickService {
 	}
 
 	// 폴더 리스트가 넘어오면, 각 폴더 내부에 있는 픽 리스트 조회
-	@LoginUserIdDistributedLock
 	@Transactional(readOnly = true)
 	public List<PickResult.FolderPickWithViewCountList> getFolderListChildPickList(PickCommand.ReadList command) {
 		return command.folderIdList().stream()
