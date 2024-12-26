cf18b5d063e64cacf28291caec63af47720d54d4
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-12-26 17:27:21 +0900

    feat: PickRecordOverlay useImageLoader 적용

--- frontend/techpick/src/components/DragOverlay/PickRecordOverlay.tsx
+++ frontend/techpick/src/components/DragOverlay/PickRecordOverlay.tsx
@@ -2,18 +2,20 @@
 
 import Image from 'next/image';
 import { ExternalLink as ExternalLinkIcon } from 'lucide-react';
+import { useImageLoader } from '@/hooks';
 import { useTagStore } from '@/stores';
 import { formatDateString } from '@/utils';
+import { pickRecordOverlayLayoutStyle } from './pickRecordOverlay.css';
+import { PickDateColumnLayout } from '../PickRecord/PickDateColumnLayout';
+import { PickImageColumnLayout } from '../PickRecord/PickImageColumnLayout';
 import {
-  pickRecordLayoutStyle,
   pickImageStyle,
   pickTitleSectionStyle,
   dateTextStyle,
-  externalLinkIconStyle,
   linkLayoutStyle,
-} from './pickRecordOverlay.css';
-import { PickDateColumnLayout } from '../PickRecord/PickDateColumnLayout';
-import { PickImageColumnLayout } from '../PickRecord/PickImageColumnLayout';
+  externalLinkIconStyle,
+  imageStyle,
+} from '../PickRecord/pickRecord.css';
 import { PickTagColumnLayout } from '../PickRecord/PickTagColumnLayout';
 import { PickTitleColumnLayout } from '../PickRecord/PickTitleColumnLayout';
 import { Separator } from '../PickRecord/Separator';
@@ -24,24 +26,26 @@ export function PickRecordOverlay({ pickInfo }: PickViewItemComponentProps) {
   const pick = pickInfo;
   const link = pickInfo.linkInfo;
   const { findTagById } = useTagStore();
+  const { imageStatus } = useImageLoader(link.imageUrl);
 
-  const filteredSelectedTagList: TagType[] = [];
-
-  pickInfo.tagIdOrderedList.forEach((tagId) => {
-    const tagInfo = findTagById(tagId);
-    if (tagInfo) {
-      filteredSelectedTagList.push(tagInfo);
-    }
-  });
+  const filteredSelectedTagList: TagType[] = pickInfo.tagIdOrderedList
+    .map((tagId) => findTagById(tagId))
+    .filter((tag): tag is TagType => tag !== undefined);
 
   return (
-    <div className={pickRecordLayoutStyle}>
+    <div className={pickRecordOverlayLayoutStyle}>
       <PickImageColumnLayout>
         <div className={pickImageStyle}>
-          {link.imageUrl ? (
-            <Image src={link.imageUrl} alt="" fill />
+          {imageStatus === 'loaded' ? (
+            <img
+              src={link.imageUrl}
+              alt=""
+              width="96px"
+              height="47.25px"
+              className={imageStyle}
+            />
           ) : (
-            <Image src={'/image/default_image.svg'} alt="" fill sizes="96px" />
+            <Image src="/image/default_image.svg" alt="" fill sizes="96px" />
           )}
         </div>
       </PickImageColumnLayout>
