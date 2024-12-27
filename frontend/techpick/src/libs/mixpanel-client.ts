ebd2ebb8a5651be4e81e255e495b481b17feb1e4
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-12-27 23:06:35 +0900

    feat: mixpannel 초기화 세팅

---
+++ frontend/techpick/src/libs/mixpanel-client.ts
@@ -0,0 +1,13 @@
+import mixpanel from 'mixpanel-browser';
+
+const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
+
+if (typeof window !== 'undefined' && MIXPANEL_TOKEN) {
+  mixpanel.init(MIXPANEL_TOKEN, {
+    debug: process.env.NODE_ENV !== 'production',
+    track_pageview: true,
+    persistence: 'localStorage',
+  });
+}
+
+export default mixpanel;
