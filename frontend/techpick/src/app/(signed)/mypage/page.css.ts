46be0cbf87493b38a76503c9c3a15cf126b58537
dmdgpdi <dmdgpdi@naver.com>
2024-12-30 20:29:11 +0900

    feat: 마이 페이지에서 바로 튜토리얼 볼 수 있게 수정

--- frontend/techpick/src/app/(signed)/mypage/page.css.ts
+++ frontend/techpick/src/app/(signed)/mypage/page.css.ts
@@ -23,20 +23,43 @@ export const myPageContentContainerLayoutStyle = style({
   justifyContent: 'space-between',
 });
 
-export const tutorialReplaySwitchLayoutStyle = style({
+export const tutorialReplayCheckboxLayoutStyle = style({
   display: 'flex',
   alignItems: 'center',
   gap: '8px',
   padding: '16px 0',
 });
 
-export const tutorialReplaySwitchLabelStyle = style({
-  fontSize: '12px',
-  cursor: 'pointer',
-  flexShrink: 0,
-});
-
 export const buttonSectionStyle = style({
   display: 'flex',
   gap: '16px',
+});
+
+export const checkboxRootStyle = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '16px',
+  height: '16px',
+  borderRadius: '4px',
+  backgroundColor: colorVars.orange1,
+  boxShadow: `0 2px 10px ${colorVars.sand7}`,
+
+  ':hover': {
+    backgroundColor: colorVars.orange3,
+  },
+
+  ':focus': {
+    boxShadow: '0 0 0 2px black',
+  },
+});
+
+export const checkboxIndicatorStyle = style({
+  color: colorVars.orange11,
+});
+
+export const tutorialReplayCheckboxLabelStyle = style({
+  fontSize: '12px',
+  cursor: 'pointer',
+  flexShrink: 0,
 });
