d54736eecc9adcbb4bbf94da7a601e4590077080
sangwonsheep <sangwonsheep@gmail.com>
2025-01-03 17:05:10 +0900

    hotfix: workflow 테스트를 위한 주석 추가

--- backend/baguni-api/src/main/java/baguni/BaguniApiApplication.java
+++ backend/baguni-api/src/main/java/baguni/BaguniApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BaguniApiApplication {
-	// 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(BaguniApiApplication.class, args);
 	}
