7199b21e818966ea8c9a9de6fa6878c1eebc10ea
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2025-01-06 11:53:13 +0900

    feat: 사용하지 않는 useHasPick 제거

--- frontend/techpick-extension/src/components/index.ts
+++ frontend/techpick-extension/src/components/index.ts
@@ -3,6 +3,4 @@ export { DeselectTagButton } from './DeselectTagButton';
 export { ToggleThemeButton } from './ToggleThemeButton';
 export { TagPicker } from './TagPicker';
 export { SkeltonPickForm } from './SkeltonPickForm';
-export { CreatePickForm } from './CreatePickForm';
 export { UpdatePickForm } from './UpdatePickForm';
-// 4. 이미 픽이 존재하면, 아이콘을 백그라운드에서 바꿔줘야한다.
