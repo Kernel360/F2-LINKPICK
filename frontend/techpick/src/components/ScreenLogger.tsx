357b96e930a2022092ead2c9ca2b9dfdd8e273f6
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2025-01-07 15:46:47 +0900

    feat: 로그인 한 유저가 페이지 방문 시에 식별

--- frontend/techpick/src/components/ScreenLogger.tsx
+++ frontend/techpick/src/components/ScreenLogger.tsx
@@ -1,21 +1,36 @@
 import type { PropsWithChildren } from 'react';
+import { cookies, headers } from 'next/headers';
+import { userAgent } from 'next/server';
+import { jwtDecode } from 'jwt-decode';
 import { mixpanelServer } from '@/libs/mixpanel-server';
 
 /**
- * @description 특정 페이지에 방문했는지 확인하는 컴포넌트입니다.
+ * @description 특정 페이지에 방문했는지 확인하는 서버 컴포넌트입니다.
  * @param eventName 해당 이벤트의 이름입니다. snake case로 명세해주세요. ex) shared_page_view
- * @param userId 인증된 사용자의 ID입니다. 없을 경우 익명 사용자로 처리됩니다.
  * @param logInfo 이벤트의 추가적인 정보를 담고 싶을 때 사용해주세요.
  */
 export function ScreenLogger({
   eventName,
-  userId = 'anonymous',
   logInfo = {},
   children,
 }: PropsWithChildren<ScreenLoggerProps>) {
+  const headersList = headers();
+  const { device, os } = userAgent({ headers: headersList });
+  const cookieStore = cookies();
+  const access_token = cookieStore.get('access_token');
+  const $user_id = access_token
+    ? jwtDecode<AccessTokenInfoType>(access_token.value).id
+    : 'anonymous';
+  const $device_id = `${device.vendor || 'unknown'}-${device.type || undefined}-${device.model || 'unknown'}`;
+  const device_type = device.type || 'unknown';
+  const $os = os.name ?? 'unknown';
+
   const properties: Record<string, unknown> = {
     ...logInfo,
-    $user_id: userId,
+    $user_id,
+    $device_id,
+    device_type,
+    $os,
   };
 
   mixpanelServer.track(eventName, properties);
@@ -25,6 +40,9 @@ export function ScreenLogger({
 
 interface ScreenLoggerProps {
   eventName: string;
-  userId?: string;
   logInfo?: object;
+}
+
+interface AccessTokenInfoType {
+  id: string;
 }
