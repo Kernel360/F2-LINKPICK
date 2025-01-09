ce3c36bbdad892e532e17139cbcf978641401aac
sangwonsheep <sangwonsheep@gmail.com>
2025-01-09 21:08:50 +0900

    refactor: selenium 옵션 제거

--- backend/baguni-common/src/main/java/baguni/common/lib/opengraph/OpenGraphReaderSelenium.java
+++ backend/baguni-common/src/main/java/baguni/common/lib/opengraph/OpenGraphReaderSelenium.java
@@ -105,12 +105,10 @@ public class OpenGraphReaderSelenium implements OpenGraphReader {
 		options.addArguments("--headless"); // 브라우저 UI 없이 백그라운드로 동작
 		options.addArguments("--user-agent=" + openGraphOption.getUserAgent());
 		options.addArguments("--no-sandbox");
-		options.addArguments("--single-process");
 		options.addArguments("--start-maximized");
 		options.addArguments("--disable-popup-blocking"); // 팝업 안뜨게
 		options.addArguments("--remote-allow-origins=*"); // 모든 출처에서의 연결을 허용, 자동화된 테스트나 CORS 제한을 우회할 때 유용
 		options.addArguments("--disable-dev-shm-usage"); // Chrome이 /dev/shm 대신 /tmp 디렉토리를 사용, /tmp는 일반적인 파일 시스템으로, 크기 제한이 없어 메모리 부족 문제를 방지
-		options.addArguments("--remote-debugging-port=9222");
 
 		// WebDriver 생성
 		return new ChromeDriver(options);
