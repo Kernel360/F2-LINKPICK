14da8f84162225796d97f55b13883f05491718dc
dmdgpdi <dmdgpdi@naver.com>
2025-01-16 21:52:51 +0900

    feat: api 호출 함수 에러 발생 일반 에러로 변환

--- frontend/techpick/src/apis/tag/deleteTag.ts
+++ frontend/techpick/src/apis/tag/deleteTag.ts
@@ -1,13 +1,23 @@
+import { HTTPError } from 'ky';
 import { apiClient } from '../apiClient';
 import { API_URLS } from '../apiConstants';
+import { returnErrorFromHTTPError } from '../error';
 import { DeleteTagRequestType } from '@/types';
 
 export const deleteTag = async (tagId: DeleteTagRequestType['id']) => {
-  const deleteTagRequest: DeleteTagRequestType = {
-    id: tagId,
-  };
+  try {
+    const deleteTagRequest: DeleteTagRequestType = {
+      id: tagId,
+    };
 
-  await apiClient.delete<never>(API_URLS.DELETE_TAGS, {
-    json: deleteTagRequest,
-  });
+    await apiClient.delete<never>(API_URLS.DELETE_TAGS, {
+      json: deleteTagRequest,
+    });
+  } catch (httpError) {
+    if (httpError instanceof HTTPError) {
+      const error = await returnErrorFromHTTPError(httpError);
+      throw error;
+    }
+    throw httpError;
+  }
 };
--- frontend/techpick/src/apis/tag/updateTag.ts
+++ frontend/techpick/src/apis/tag/updateTag.ts
@@ -1,9 +1,19 @@
+import { HTTPError } from 'ky';
 import { apiClient } from '../apiClient';
 import { API_URLS } from '../apiConstants';
+import { returnErrorFromHTTPError } from '../error';
 import { UpdateTagRequestType, UpdateTagResponseType } from '@/types';
 
 export const updateTag = async (updateTag: UpdateTagRequestType) => {
-  await apiClient.patch<UpdateTagResponseType>(API_URLS.UPDATE_TAGS, {
-    json: updateTag,
-  });
+  try {
+    await apiClient.patch<UpdateTagResponseType>(API_URLS.UPDATE_TAGS, {
+      json: updateTag,
+    });
+  } catch (httpError) {
+    if (httpError instanceof HTTPError) {
+      const error = await returnErrorFromHTTPError(httpError);
+      throw error;
+    }
+    throw httpError;
+  }
 };
