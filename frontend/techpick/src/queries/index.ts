b6a9d10de2e152e5e071a18cc65b004152d453c3
dmdgpdi <dmdgpdi@naver.com>
2025-01-16 23:15:16 +0900

    feat: 서버에서도 apiClient 동작하게 변경

--- frontend/techpick/src/queries/index.ts
+++ frontend/techpick/src/queries/index.ts
@@ -2,3 +2,4 @@ export { useCreateTag } from './useCreateTag';
 export { useDeleteTag } from './useDeleteTag';
 export { useFetchTagList } from './useFetchTagList';
 export { useUpdateTag } from './useUpdateTag';
+export { tagKeys } from './tagKeys';
