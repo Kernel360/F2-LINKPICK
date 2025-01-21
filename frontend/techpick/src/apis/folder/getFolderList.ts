cf95221e9445c2fc57e94e6df37677002470c6c0
dmdgpdi <dmdgpdi@naver.com>
2025-01-21 15:11:13 +0900

    refactor: 타입 변경에 따른 폴더 관련 API 함수 적용

--- frontend/techpick/src/apis/folder/getFolderList.ts
+++ frontend/techpick/src/apis/folder/getFolderList.ts
@@ -1,16 +1,17 @@
-import type { GetShareFolderByIdResponseType } from '@/types/GetShareFolderByIdResponseType';
+import type { GetFolderListResponseType } from '@/types/GetFolderListResponseType';
 import { HTTPError } from 'ky';
 import { apiClient } from '../apiClient';
 import { API_URLS } from '../apiConstants';
 import { returnErrorFromHTTPError } from '../error';
 
-export const getShareFolderById = async (uuid: string) => {
+export const getRootFolderList = async () => {
   try {
-    const response = await apiClient.get<GetShareFolderByIdResponseType>(
-      API_URLS.GET_SHARED_FOLER_BY_UUID(uuid),
+    const response = await apiClient.get<GetFolderListResponseType>(
+      API_URLS.GET_FOLDERS,
     );
+    const data = await response.json();
 
-    return response.json();
+    return data;
   } catch (httpError) {
     if (httpError instanceof HTTPError) {
       const error = await returnErrorFromHTTPError(httpError);
