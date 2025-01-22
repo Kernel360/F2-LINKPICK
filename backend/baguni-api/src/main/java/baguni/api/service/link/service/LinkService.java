ec894b72980f5c96df678704f9b438f1803ae058
Minky <minkyeki@student.42seoul.kr>
2025-01-22 17:00:40 +0900

    [FEAT] 유명 블로그 글 소개 API  (#998)
    * chore: 패키지 명을 ranking 에서 suggestion으로 변경
    
    * feat: 블로그 글 획득 API 구현
    
    * feat: 6시간 캐싱 처리

--- backend/baguni-api/src/main/java/baguni/api/service/link/service/LinkService.java
+++ backend/baguni-api/src/main/java/baguni/api/service/link/service/LinkService.java
@@ -1,8 +1,14 @@
 package baguni.api.service.link.service;
 
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import baguni.common.annotation.MeasureTime;
+import baguni.common.lib.cache.CacheType;
+import baguni.domain.infrastructure.link.dto.RssLinkInfo;
 import baguni.domain.model.link.Link;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,5 +34,15 @@ public class LinkService {
 	public LinkInfo saveLink(String url) {
 		Link link = linkDataHandler.getOptionalLink(url).orElseGet(() -> Link.createLink(url));
 		return linkMapper.of(linkDataHandler.saveLink(link));
+	}
+
+	@MeasureTime
+	@Transactional(readOnly = true)
+	@Cacheable(cacheNames = CacheType.CACHE_NAME.DAILY_RSS_BLOG_ARTICLE)
+	public List<RssLinkInfo> getRssLinkList(int limit) {
+		return linkDataHandler.getRssLinkList(limit)
+							  .stream()
+							  .map(linkMapper::toRssLinkInfo)
+							  .toList();
 	}
 }
--- backend/baguni-common/src/main/java/baguni/common/lib/cache/CacheType.java
+++ backend/baguni-common/src/main/java/baguni/common/lib/cache/CacheType.java
@@ -1,12 +1,15 @@
-package baguni.ranking.cache;
+package baguni.common.lib.cache;
 
 import lombok.Getter;
 
 @Getter
 public enum CacheType {
+	// ---------------------------------------------------
 	DAILY_LINK_RANK(CACHE_NAME.DAILY_LINK_RANK, 60 * 60, 10000), // 1시간
 	WEEKLY_LINK_RANK(CACHE_NAME.WEEKLY_LINK_RANK, 24 * 60 * 60, 10000), // 24시간
-	MONTHLY_PICK_RANK(CACHE_NAME.MONTHLY_PICK_RANK, 3 * 60 * 60, 10000); // 3시간
+	MONTHLY_PICK_RANK(CACHE_NAME.MONTHLY_PICK_RANK, 3 * 60 * 60, 10000), // 3시간
+	// ---------------------------------------------------
+	DAILY_RSS_BLOG_ARTICLE(CACHE_NAME.MONTHLY_PICK_RANK, 6 * 60 * 60, 10000); // 6시간
 
 	CacheType(String cacheName, int expireAfterWrite, int maximumSize) {
 		this.cacheName = cacheName;
@@ -19,9 +22,12 @@ public enum CacheType {
 	private final int maximumSize;
 
 	public static class CACHE_NAME {
+		// ---------------------------------------------------
 		public static final String DAILY_LINK_RANK = "daily_link_rank";
 		public static final String WEEKLY_LINK_RANK = "weekly_link_rank";
 		public static final String MONTHLY_PICK_RANK = "monthly_pick_rank";
+		// ---------------------------------------------------
+		public static final String DAILY_RSS_BLOG_ARTICLE = "daily_rss_blog_article";
 	}
 
 }
