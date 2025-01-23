2b51f76bff0455cf52e71cabf70d609d6b7693e2
dmdgpdi <dmdgpdi@naver.com>
2025-01-23 18:54:26 +0900

    refactor: FolderItemOverlay 내부 컴포넌트 변경

--- frontend/techpick/src/components/DragOverlay/FolderItemOverlay.tsx
+++ frontend/techpick/src/components/DragOverlay/FolderItemOverlay.tsx
@@ -1,16 +1,16 @@
 import { FolderOpen as FolderOpenIcon } from 'lucide-react';
-import {
-  FolderIconStyle,
-  folderTextStyle,
-} from '../FolderTree/folderLinkItem.css';
+import { NavigationItem } from '../SideNavigationBar/NavigationItem';
 import { folderItemOverlay } from './folderItemOverlay.css';
 
 export function FolderItemOverlay({ name }: FolderItemOverlayProps) {
   return (
-    <div className={folderItemOverlay}>
-      <FolderOpenIcon className={FolderIconStyle} />
-      <p className={folderTextStyle}>{name}</p>
-    </div>
+    <NavigationItem
+      href="#"
+      text={name}
+      icon={FolderOpenIcon}
+      isActive={false}
+      className={folderItemOverlay}
+    />
   );
 }
 
