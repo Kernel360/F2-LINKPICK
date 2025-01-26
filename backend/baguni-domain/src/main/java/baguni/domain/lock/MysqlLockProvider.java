2dd063e607c4a80181bff84d97b0e81943942755
sangwonsheep <sangwonsheep@gmail.com>
2025-01-26 22:06:02 +0900

    refactor: 락 설정, 해제 시 sessionId 추적

--- backend/baguni-domain/src/main/java/baguni/domain/lock/MysqlLockProvider.java
+++ backend/baguni-domain/src/main/java/baguni/domain/lock/MysqlLockProvider.java
@@ -35,9 +35,9 @@ public class MysqlLockProvider implements LockProvider {
 		String lockKey = userId + "";
 		Boolean result = jdbcTemplate.queryForObject(sql, Boolean.class, lockKey);
 		if (!Boolean.TRUE.equals(result)) {
-			String lockConnectionSql = "SELECT IS_USED_LOCK(" + lockKey + ")";
+			String lockConnectionSql = "SELECT IS_USED_LOCK(?)";
 			String currentConnectionSql = "SELECT CONNECTION_ID()";
-			String lockConnectionId = jdbcTemplate.queryForObject(lockConnectionSql, String.class);
+			String lockConnectionId = jdbcTemplate.queryForObject(lockConnectionSql, String.class, lockKey);
 			String currentConnectionId = jdbcTemplate.queryForObject(currentConnectionSql, String.class);
 			log.error("[락 해제 실패] Lock 설정 sessionId : {}, 현재 실행 중인 sessionId : {}", lockConnectionId, currentConnectionId);
 			throw new LockException("락 해제 실패 : " + lockKey);
