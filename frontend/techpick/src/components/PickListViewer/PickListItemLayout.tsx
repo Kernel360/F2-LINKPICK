92845bf76668331202b3cd253c71c36c012ea3f9
dmdgpdi <dmdgpdi@naver.com>
2025-01-28 17:08:23 +0900

    feat: 현재 사용하지 않는 픽 렌더링 컴포넌트 제거

--- frontend/techpick/src/components/PickListViewer/PickListItemLayout.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import type { PickViewItemListLayoutComponentProps } from './PickListViewer';
-import { pickListItemLayoutStyle } from './pickListItemLayout.css';
-
-export function PickListItemLayout({
-  children,
-}: PickViewItemListLayoutComponentProps) {
-  return <div className={pickListItemLayoutStyle}>{children}</div>;
-}
