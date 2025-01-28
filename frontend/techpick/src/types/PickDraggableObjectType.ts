e548f77ac5819e8956ffcd490aa0d389eb3944dd
dmdgpdi <dmdgpdi@naver.com>
2025-01-28 17:15:39 +0900

    feat: 픽 Drag시 해당 Pick에 대한 정보도 포함하는지 검증 타입 추가

--- frontend/techpick/src/types/PickDraggableObjectType.ts
+++ frontend/techpick/src/types/PickDraggableObjectType.ts
@@ -1,9 +1,11 @@
 import type { UniqueIdentifier } from '@dnd-kit/core';
 import type { DnDCurrentType } from './DnDCurrentType';
+import type { PickInfoType } from './PickInfoType';
 
 export interface PickDraggableObjectType extends DnDCurrentType {
   type: 'pick';
   parentFolderId: number;
+  pickInfo: PickInfoType;
   sortable: {
     containerId: string | null;
     items: UniqueIdentifier[];
