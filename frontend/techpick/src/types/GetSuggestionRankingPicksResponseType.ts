0716eaf4b63866f43db07a09fef7b9f1552800dd
dmdgpdi <dmdgpdi@naver.com>
2025-02-03 19:24:31 +0900

    feat: 기술 블로그 글 요청 API 함수 작성

--- frontend/techpick/src/types/GetSuggestionRankingPicksResponseType.ts
+++ frontend/techpick/src/types/GetSuggestionRankingPicksResponseType.ts
@@ -3,12 +3,12 @@ import type { ConcreteType } from './ConcreteType';
 
 export interface GetSuggestionRankingPicksResponseType {
   dailyViewRanking: ConcreteType<
-    components['schemas']['baguni.api.application.ranking.dto.LinkInfoWithCount']
+    components['schemas']['baguni.api.application.suggestion.dto.LinkInfoWithCount']
   >[];
   weeklyViewRanking: ConcreteType<
-    components['schemas']['baguni.api.application.ranking.dto.LinkInfoWithCount']
+    components['schemas']['baguni.api.application.suggestion.dto.LinkInfoWithCount']
   >[];
   monthlyPickRanking: ConcreteType<
-    components['schemas']['baguni.api.application.ranking.dto.LinkInfoWithCount']
+    components['schemas']['baguni.api.application.suggestion.dto.LinkInfoWithCount']
   >[];
 }
