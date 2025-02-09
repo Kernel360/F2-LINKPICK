36241f56135c77e9d4393b209f92c6a22ae7b251
dmdgpdi <dmdgpdi@naver.com>
2025-02-09 15:59:34 +0900

    design: 무한 스크롤과 헤더 사이의 간격 변경

--- frontend/techpick/src/app/(signed)/folders/[folderId]/MobileFolderDetailPage.tsx
+++ frontend/techpick/src/app/(signed)/folders/[folderId]/MobileFolderDetailPage.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { MobilePickInfiniteScrollList } from '@/components/MobilePickInfiniteScrollList';
 import { useParams } from 'next/navigation';
+import { mobileFolderDetailPageStyle } from './mobileFolderDetailPage.css';
 
 export function MobileFolderDetailPage() {
   const { folderId: stringFolderId } = useParams<{ folderId: string }>();
   const folderId = Number(stringFolderId);
 
   return (
-    <div>
+    <div className={mobileFolderDetailPageStyle}>
       <MobilePickInfiniteScrollList folderId={folderId} />
     </div>
   );
---
+++ frontend/techpick/src/app/(signed)/folders/[folderId]/mobileFolderDetailPage.css.ts
@@ -0,0 +1,5 @@
+import { style } from '@vanilla-extract/css';
+
+export const mobileFolderDetailPageStyle = style({
+  marginTop: '8px',
+});
