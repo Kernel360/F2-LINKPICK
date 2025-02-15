bc7bc91d3f7cfa3f242b559a09854e9c43f14591
dmdgpdi <dmdgpdi@naver.com>
2025-02-15 18:28:25 +0900

    fix: og image 생성 api 로직 변경

--- frontend/techpick/src/app/api/generate-og-image/route.tsx
+++ frontend/techpick/src/app/api/generate-og-image/route.tsx
@@ -12,6 +12,17 @@ const styles = {
   16: { width: '300px', height: '157.5px' },
 };
 
+async function isValidImageUrl(url: string): Promise<boolean> {
+  try {
+    const res = await fetch(url, { method: 'HEAD' });
+    return (
+      res.ok && res.headers.get('Content-Type')?.startsWith('image/') === true
+    );
+  } catch {
+    return false;
+  }
+}
+
 const getImageStyle = (index: number) => {
   return styles[index as keyof typeof styles] || styles[16];
 };
@@ -23,24 +34,17 @@ export async function GET(req: NextRequest) {
   const width = 1200;
   const height = 630;
 
-  // 이미지 개수를 1, 2, 4, 8, 16 중 가장 가까운 수로 조정
+  const validImageUrls = await Promise.all(
+    imageUrls.map(async (url) => ((await isValidImageUrl(url)) ? url : null)),
+  );
+  const filteredImageUrls = validImageUrls.filter((url) => url !== null);
   const adjustedCount = [1, 2, 4, 8, 16].reduce((prev, curr) =>
-    Math.abs(curr - imageUrls.length) < Math.abs(prev - imageUrls.length)
+    Math.abs(curr - filteredImageUrls.length) <
+    Math.abs(prev - filteredImageUrls.length)
       ? curr
       : prev,
   );
-
-  const images = await Promise.all(
-    imageUrls.slice(0, adjustedCount).map(async (url: string) => {
-      try {
-        const res = await fetch(url);
-        if (!res.ok) throw new Error('Failed to fetch image');
-        return url;
-      } catch {
-        return '/image/og_image.png';
-      }
-    }),
-  );
+  const images = filteredImageUrls.slice(0, adjustedCount);
   const imageCount = images.length;
 
   return new ImageResponse(
