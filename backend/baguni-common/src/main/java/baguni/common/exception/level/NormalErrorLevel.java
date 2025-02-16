2d44485741e85b761405d27b0cf93c0e962372e2
kimminkyeu <minkyeki@student.42seoul.kr>
2025-02-16 16:31:39 +0900

    fix: Normal 로그 레벨 수정

--- backend/baguni-common/src/main/java/baguni/common/exception/level/NormalErrorLevel.java
+++ backend/baguni-common/src/main/java/baguni/common/exception/level/NormalErrorLevel.java
@@ -1,7 +1,6 @@
 package baguni.common.exception.level;
 
 import lombok.extern.slf4j.Slf4j;
-import baguni.common.exception.base.ApiException;
 import baguni.common.util.CachedHttpServletRequest;
 
 @Slf4j
@@ -9,11 +8,11 @@ public class NormalErrorLevel extends ErrorLevel {
 
 	@Override
 	public void handleError(Exception exception, CachedHttpServletRequest request) {
-		log.error("{}{}", exception.getMessage(), request); // stack trace 미출력
+		log.info("{}{}", exception.getMessage(), request); // stack trace 미출력
 	}
 
 	@Override
 	public void handleError(Exception exception) {
-		log.error(exception.getMessage(), exception);
+		log.info(exception.getMessage(), exception);
 	}
 }
